c7be57a8efc4aee027f129c779b6adbad22c862f
vvo <vincent.voyer@algolia.com>
2017-04-04 15:51:04 +0200

    Updates

---
+++ react-instantsearch/examples/default-theme/index-build-8e7e93f0aa6f3884e7aba93293675db8.js
@@ -0,0 +1,7 @@
+webpackJsonp([3,0,1,2,4,5,6],{116:function(t,r){},17:function(t,r,e){"use strict";function n(t){if(null===t||void 0===t)throw new TypeError("Object.assign cannot be called with null or undefined");return Object(t)}function o(){try{if(!Object.assign)return!1;var t=new String("abc");if(t[5]="de","5"===Object.getOwnPropertyNames(t)[0])return!1;for(var r={},e=0;e<10;e++)r["_"+String.fromCharCode(e)]=e;var n=Object.getOwnPropertyNames(r).map(function(t){return r[t]});if("0123456789"!==n.join(""))return!1;var o={};return"abcdefghijklmnopqrst".split("").forEach(function(t){o[t]=t}),"abcdefghijklmnopqrst"===Object.keys(Object.assign({},o)).join("")}catch(t){return!1}}/*
+object-assign
+(c) Sindre Sorhus
+@license MIT
+*/
+var c=Object.getOwnPropertySymbols,i=Object.prototype.hasOwnProperty,a=Object.prototype.propertyIsEnumerable;t.exports=o()?Object.assign:function(t,r){for(var e,o,s=n(t),u=1;u<arguments.length;u++){e=Object(arguments[u]);for(var f in e)i.call(e,f)&&(s[f]=e[f]);if(c){o=c(e);for(var p=0;p<o.length;p++)a.call(e,o[p])&&(s[o[p]]=e[o[p]])}}return s}},85:function(t,r,e){"use strict";t.exports=e(88)},9:function(t,r,e){"use strict";t.exports=e(40)}},[1192]);
+//# sourceMappingURL=index-build.js.map
