88f2f1fd01dffe2c3de0d3549a4ad34af1bf91a2
Haroen Viaene <hello@haroen.me>
2017-04-13 16:00:32 +0200

    Revert "exit with a non-zero code to test if build fails"
    This reverts commit de118650560294c7a8019e227ccf232f5521c0bf.

--- docgen/build.js
+++ docgen/build.js
@@ -2,8 +2,6 @@ import builder from './builder.js';
 import revAssets from './plugins/rev-assets.js';
 import { build as middlewares } from './middlewares';
 
-process.exit(1);
-
 builder(
   {
     middlewares,
