de118650560294c7a8019e227ccf232f5521c0bf
Haroen Viaene <hello@haroen.me>
2017-04-13 14:06:55 +0200

    exit with a non-zero code to test if build fails

--- docgen/build.js
+++ docgen/build.js
@@ -2,6 +2,8 @@ import builder from './builder.js';
 import revAssets from './plugins/rev-assets.js';
 import { build as middlewares } from './middlewares';
 
+process.exit(1);
+
 builder(
   {
     middlewares,
