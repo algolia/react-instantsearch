485d4cfa3f24a501522f3194db34619d44fb6501
mthuret <marielaure.thuret@algolia.com>
2017-07-12 12:56:51 +0200

    chore(v4.0): update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -27,8 +27,8 @@ if [[ $(cd packages/react-instantsearch-theme-algolia && npm owner ls) != *"$(np
 fi
 
 currentBranch=`git rev-parse --abbrev-ref HEAD`
-if [ $currentBranch != 'master' ]; then
-  printf "Release: You must be on master\n"
+if [ $currentBranch != 'v4.0' ]; then
+  printf "Release: You must be on v4.0\n"
   exit 1
 fi
 
@@ -38,7 +38,7 @@ if [[ -n $(git status --porcelain) ]]; then
 fi
 
 # printf "\n\nRelease: update working tree"
-git pull origin master
+git pull origin v4.0
 git fetch origin --tags
 
 # printf "Release: install dependencies"
@@ -97,7 +97,7 @@ git tag "v$newVersion"
 printf "\n\nRelease: almost done, check everything in another terminal tab if you want.\n"
 read -p "=> Release: when ready, press [ENTER] to push to github and publish the package"
 
-git push origin master
+git push origin v4.0
 git push origin --tags
 
 printf "\n\nRelease: pushed to github, publish on npm"
