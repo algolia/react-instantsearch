b6ed9b6f5a229ca0973d2d84dc307340a18a6a38
Marie-Laure Sin <marie-laure.sin@algolia.com>
2017-07-17 10:55:11 +0200

    feat(Breadcrumb): improve assembleBreadcrumb method

--- packages/react-instantsearch/src/connectors/connectBreadcrumb.js
+++ packages/react-instantsearch/src/connectors/connectBreadcrumb.js
@@ -26,23 +26,25 @@ export default createConnector({
       searchResults
     );
 
-    const assembleBreadcrumb = function(items, previous) {
+    const assembleBreadcrumb = (items, previous) => {
       return !items
         ? []
-        : items.reduce(function(acc, item) {
+        : items.reduce((acc, item) => {
             if (item.isRefined === true) {
               acc.push({
                 label: item.label,
+                // It it's a nested "items", "value" is equal to the previous value concatenated with the current label
+                // If it's the first level, it is equal to the current label
                 value: previous
                   ? `${previous[previous.length - 1].value} > ${item.label}`
                   : item.label,
               });
+              // Create a variable in order to keep the same acc for the recursion, otherwise reduce returns a new one
               acc = acc.concat(assembleBreadcrumb(item.items, acc));
             }
             return acc;
           }, []);
     };
-
     return { items: assembleBreadcrumb(newProps.items) };
   },
 
