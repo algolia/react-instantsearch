167f9fb4b9754ddb2db346c748575fa3cd2b7071
Lucas Bonomi <lucas.bonomi@gmail.com>
2017-08-10 11:15:58 +0200

    New design

--- docgen/config.js
+++ docgen/config.js
@@ -1,16 +1,29 @@
+var algoliaComponents = require('algolia-frontend-components');
+var fs = require('fs');
+
 import pkg from '../packages/react-instantsearch/package.json';
 import { rootPath } from './path';
+
+var content = JSON.parse(fs.readFileSync('./docgen/src/data/communityHeader.json', 'utf8').toString());
+var headerAlgoliaLogo = fs.readFileSync('./docgen/assets/img/algolia-logo-whitebg.svg', 'utf8').toString();
+var headerCommunityLogo = fs.readFileSync('./docgen/assets/img/algolia-community-dark.svg', 'utf8').toString();
+var header = algoliaComponents.communityHeader(content, {
+  algoliaLogo: headerAlgoliaLogo,
+  communityLogo: headerCommunityLogo
+});
 
 const configs = {
   production: {
     docsDist: rootPath('docs-production/react-instantsearch'),
     storyBookPublicPath: 'storybook/',
     publicPath: '/react-instantsearch/',
+    header: header
   },
   development: {
     docsDist: rootPath('docs/react-instantsearch'),
     storyBookPublicPath: 'http://localhost:6006/',
     publicPath: '/',
+    header: header
   },
 };
 
