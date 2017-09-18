e939604f2efa254d0d3f56267d1c515a63f38fc4
Marie-Laure Thuret <marielaure.thuret@algolia.com>
2017-09-18 15:23:11 +0200

    wip

--- docgen/config.js
+++ docgen/config.js
@@ -1,15 +1,21 @@
-var algoliaComponents = require('algolia-frontend-components');
-var fs = require('fs');
+const algoliaComponents = require('algolia-frontend-components');
+const fs = require('fs');
 
 import pkg from '../packages/react-instantsearch/package.json';
 import { rootPath } from './path';
 
-var content = JSON.parse(fs.readFileSync('./docgen/src/data/communityHeader.json', 'utf8').toString());
-var headerAlgoliaLogo = fs.readFileSync('./docgen/assets/img/algolia-logo-whitebg.svg', 'utf8').toString();
-var headerCommunityLogo = fs.readFileSync('./docgen/assets/img/algolia-community-dark.svg', 'utf8').toString();
-var header = algoliaComponents.communityHeader(content, {
+const content = JSON.parse(
+  fs.readFileSync('./docgen/src/data/communityHeader.json', 'utf8').toString()
+);
+const headerAlgoliaLogo = fs
+  .readFileSync('./docgen/assets/img/algolia-logo-whitebg.svg', 'utf8')
+  .toString();
+const headerCommunityLogo = fs
+  .readFileSync('./docgen/assets/img/algolia-community-dark.svg', 'utf8')
+  .toString();
+const header = algoliaComponents.communityHeader(content, {
   algoliaLogo: headerAlgoliaLogo,
-  communityLogo: headerCommunityLogo
+  communityLogo: headerCommunityLogo,
 });
 
 const configs = {
@@ -17,13 +23,13 @@ const configs = {
     docsDist: rootPath('docs-production/react-instantsearch'),
     storyBookPublicPath: 'storybook/',
     publicPath: '/react-instantsearch/',
-    header: header
+    header,
   },
   development: {
     docsDist: rootPath('docs/react-instantsearch'),
     storyBookPublicPath: 'http://localhost:6006/',
     publicPath: '/',
-    header: header
+    header,
   },
 };
 
