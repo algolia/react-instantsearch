d32d20e1bf8a61ffc670ee2c1741bc7c7c5e1703
Lucas Bonomi <lucas.bonomi@gmail.com>
2017-09-18 13:33:12 +0200

    New doc ready

--- docgen/syntaxHighlighting.js
+++ docgen/syntaxHighlighting.js
@@ -11,6 +11,10 @@ export default function highlight(source, lang) {
   if (lang === 'html') {
     lang = 'htmlmixed';
   }
+  if (lang === 'shell') {
+    lang = 'shell';
+  }
+
 
   const heading = lang === 'shell' ? 'Command' : 'Code';
 
@@ -23,11 +27,8 @@ export default function highlight(source, lang) {
       return;
     }
 
-    tokenizedSource += `<span class="cm-${style.replace(
-      / +/g,
-      ' cm-'
-    )}">${text}</span>`;
+    tokenizedSource += `<span class="cm-${style.replace(/ +/g,' cm-')}">${text}</span>`;
   });
 
-  return `<pre class="code-sample cm-s-mdn-like codeMirror"><div class="code-wrap"><code>${tokenizedSource}</code></div></pre>`;
+  return `<pre class="code-sample cm-s-mdn-like codeMirror ${lang}"><div class="code-wrap"><code>${tokenizedSource}</code></div></pre>`;
 }
