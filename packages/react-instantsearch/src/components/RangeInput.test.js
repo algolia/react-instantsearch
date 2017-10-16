1d85b25ef9209b02572dee5b855ca4f6a833be77
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-10-16 16:56:31 +0200

    chore: add enzyme to json to serializer

--- packages/react-instantsearch/src/components/RangeInput.test.js
+++ packages/react-instantsearch/src/components/RangeInput.test.js
@@ -3,11 +3,9 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import serializer from 'enzyme-to-json/serializer';
 import RangeInput, { RawRangeInput } from './RangeInput';
 
 Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(serializer);
 
 describe('RangeInput', () => {
   const shallowRender = (props = {}, context = {}) => {
