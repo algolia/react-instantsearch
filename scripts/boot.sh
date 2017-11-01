174939b58e6b3c6d45f1c2020585e87810261f5c
Haroen Viaene <fingebimus@me.com>
2017-11-01 10:24:41 +0100

    😶

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -2,10 +2,10 @@
 
 # Use this script to install dependencies of every packages
 rm -rf node_modules packages/react-instantsearch/node_modules packages/react-instantsearch-theme-algolia/node_modules &&
-NODE_ENV=development yarn --ignore-engines &&
+NODE_ENV=development yarn &&
 (cd node_modules/react && yarn link) &&
 (cd node_modules/react-dom && yarn link) &&
-(cd packages/react-instantsearch && yarn --ignore-engines && yarn link && yarn link react && yarn link react-dom) &&
+(cd packages/react-instantsearch && yarn && yarn link && yarn link react && yarn link react-dom) &&
 (cd packages/react-instantsearch-theme-algolia && yarn && yarn link && yarn link react && yarn link react-dom) &&
 yarn link react-instantsearch &&
 yarn link react-instantsearch-theme-algolia
