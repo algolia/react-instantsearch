513b2c6ab1d22ee28670933715f4c52a068723e2
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-11-13 15:10:04 +0100

    fix: add a specfic shim for the react-native example only

--- shim.js
+++ shim.js
@@ -1,11 +1,3 @@
-// The XMLHttpRequest is required since the AlogliaClient use it globally
-// Since `react-native@0.49.x` the `InitializeCore` module is not executed anymore
-// and so the polyfills are not applied in Jest environment.
-// see on 0.48.4: https://github.com/facebook/react-native/blob/v0.48.4/jest/setup.js#L37
-// see on 0.49.x: https://github.com/facebook/react-native/blob/v0.49.0/jest/setup.js#L37
-// eslint-disable-next-line
-global.XMLHttpRequest = require('XMLHttpRequest');
-
 // see: https://github.com/facebookincubator/create-react-app/issues/3199
 global.requestAnimationFrame = callback => {
   setTimeout(callback, 0);
