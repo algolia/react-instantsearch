20907ec277d9ecf5b76d242a2fae715f857068a9
Ronan Levesque <ronan.levesque@algolia.com>
2017-12-13 17:12:29 +0100

    Fix SearchBox

--- packages/react-instantsearch/src/components/SearchBox.js
+++ packages/react-instantsearch/src/components/SearchBox.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 
 import translatable from '../core/translatable';
 
-const DefaultLoadingIndicator = () => (
+const DefaultLoadingIndicator = cx => (
   <svg
     width="18"
     height="18"
     viewBox="0 0 38 38"
     xmlns="http://www.w3.org/2000/svg"
     stroke="#BFC7D8"
+    className={cx('loadingIcon')}
   >
     <g fill="none" fillRule="evenodd">
       <g transform="translate(1 1)" strokeWidth="2">
@@ -29,15 +30,27 @@ const DefaultLoadingIndicator = () => (
   </svg>
 );
 
-const DefaultReset = () => (
-  <svg role="img" width="1em" height="1em">
-    <use xlinkHref="#sbx-icon-clear-3" />
+const DefaultReset = cx => (
+  <svg
+    className={cx('resetIcon')}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+    width="10"
+    height="10"
+  >
+    <path d="M8.114 10L.944 2.83 0 1.885 1.886 0l.943.943L10 8.113l7.17-7.17.944-.943L20 1.886l-.943.943-7.17 7.17 7.17 7.17.943.944L18.114 20l-.943-.943-7.17-7.17-7.17 7.17-.944.943L0 18.114l.943-.943L8.113 10z" />
   </svg>
 );
 
-const DefaultSubmit = () => (
-  <svg role="img" width="1em" height="1em">
-    <use xlinkHref="#sbx-icon-search-13" />
+const DefaultSubmit = cx => (
+  <svg
+    className={cx('submitIcon')}
+    xmlns="http://www.w3.org/2000/svg"
+    width="10"
+    height="10"
+    viewBox="0 0 40 40"
+  >
+    <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z" />
   </svg>
 );
 
@@ -48,9 +61,9 @@ class SearchBox extends Component {
     refine: PropTypes.func.isRequired,
     translate: PropTypes.func.isRequired,
 
-    loadingIndicatorComponent: PropTypes.element,
-    resetComponent: PropTypes.element,
-    submitComponent: PropTypes.element,
+    renderLoadingIndicatorComponent: PropTypes.element,
+    renderResetComponent: PropTypes.element,
+    renderSubmitComponent: PropTypes.element,
 
     focusShortcuts: PropTypes.arrayOf(
       PropTypes.oneOfType([PropTypes.string, PropTypes.number])
@@ -80,9 +93,9 @@ class SearchBox extends Component {
     searchAsYouType: true,
     showLoadingIndicator: false,
     isSearchStalled: false,
-    loadingIndicatorComponent: <DefaultLoadingIndicator />,
-    submitComponent: <DefaultSubmit />,
-    resetComponent: <DefaultReset />,
+    renderLoadingIndicatorComponent: DefaultLoadingIndicator,
+    renderSubmitComponent: DefaultSubmit,
+    renderResetComponent: DefaultReset,
   };
 
   constructor(props) {
@@ -208,37 +221,11 @@ class SearchBox extends Component {
       autoFocus,
       header,
       footer,
-      loadingIndicatorComponent,
+      renderLoadingIndicatorComponent,
+      renderSubmitComponent,
+      renderResetComponent,
     } = this.props;
     const query = this.getQuery();
-
-    const submitComponent = this.props.submitComponent ? (
-      this.props.submitComponent
-    ) : (
-      <svg
-        className={cx('submitIcon')}
-        xmlns="http://www.w3.org/2000/svg"
-        width="10"
-        height="10"
-        viewBox="0 0 40 40"
-      >
-        <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z" />
-      </svg>
-    );
-
-    const resetComponent = this.props.resetComponent ? (
-      this.props.resetComponent
-    ) : (
-      <svg
-        className={cx('resetIcon')}
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        width="10"
-        height="10"
-      >
-        <path d="M8.114 10L.944 2.83 0 1.885 1.886 0l.943.943L10 8.113l7.17-7.17.944-.943L20 1.886l-.943.943-7.17 7.17 7.17 7.17.943.944L18.114 20l-.943-.943-7.17-7.17-7.17 7.17-.944.943L0 18.114l.943-.943L8.113 10z" />
-      </svg>
-    );
 
     const searchInputEvents = Object.keys(this.props).reduce((props, prop) => {
       if (
@@ -289,7 +276,7 @@ class SearchBox extends Component {
             }}
             className={cx('loadingIndicator')}
           >
-            {loadingIndicatorComponent}
+            {renderLoadingIndicatorComponent(cx)}
           </div>
         )}
         <button
@@ -298,16 +285,16 @@ class SearchBox extends Component {
           className={cx('submit')}
           hidden={isSearchStalled}
         >
-          {submitComponent}
+          {renderSubmitComponent(cx)}
         </button>
         <button
           type="reset"
           title={translate('resetTitle')}
           className={cx('reset')}
           onClick={this.onReset}
-          style={query ? { display: 'block' } : { display: 'none' }}
+          style={{ display: `${query ? 'block' : 'none'}` }}
         >
-          {resetComponent}
+          {renderResetComponent(cx)}
         </button>
       </form>
     );
