cf62300005b9aab0706309d4ca4a76764841c152
Haroen Viaene <hello@haroen.me>
2017-12-19 17:40:41 +0100

    fix: remove moved code

--- packages/react-instantsearch/src/widgets/Index.js
+++ packages/react-instantsearch/src/widgets/Index.js
@@ -1,12 +1,5 @@
-import createInstantSearch from '../core/createInstantSearch';
 import createIndex from '../core/createIndex';
-import algoliasearch from 'algoliasearch/lite';
 
-const InstantSearch = createInstantSearch(algoliasearch, {
-  Root: 'div',
-  props: { className: 'ais-InstantSearch__root' },
-});
-export { InstantSearch };
 const Index = createIndex({
   Root: 'div',
   props: { className: 'ais-MultiIndex__root' },
--- packages/react-instantsearch/src/widgets/Index.native.js
+++ packages/react-instantsearch/src/widgets/Index.native.js
@@ -1,12 +1,6 @@
-import algoliasearch from 'algoliasearch/reactnative';
-import createInstantSearch from '../core/createInstantSearch';
 import createIndex from '../core/createIndex';
 import { View } from 'react-native';
 
-const InstantSearch = createInstantSearch(algoliasearch, {
-  Root: View,
-});
-export { InstantSearch };
 const Index = createIndex({
   Root: View,
 });
