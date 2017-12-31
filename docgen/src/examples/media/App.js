1987d9024326b245736190a43c0418ac2446a268
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-31 12:36:19 +0100

    refactor: migrate media example on IS.css

--- docgen/src/examples/media/App.js
+++ docgen/src/examples/media/App.js
@@ -1,8 +1,6 @@
 /* eslint react/prop-types: 0 */
 
 import React from 'react';
-import 'react-instantsearch-theme-algolia/style.scss';
-
 import {
   InstantSearch,
   Hits,
@@ -12,11 +10,12 @@ import {
   Highlight,
   Configure,
 } from 'react-instantsearch/dom';
-
 import {
   connectSearchBox,
   connectRefinementList,
 } from 'react-instantsearch/connectors';
+import 'instantsearch.css/themes/reset.css';
+import 'instantsearch.css/themes/algolia.css';
 import { withUrlSync } from '../urlSync';
 
 const App = props => (
