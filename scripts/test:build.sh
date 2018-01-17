dc78e333eeb0a7cde7184381bbc17eb5d0d6d6ec
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-01-17 17:04:49 +0100

    chore: explicitly set error

--- scripts/test:build.sh
+++ scripts/test:build.sh
@@ -3,5 +3,7 @@
 # This script is used to test the build steps of our packages
 # to ensure we will be able to release new versions
 
-(cd packages/react-instantsearch && yarn build) &&
+set -e # exit when error
+
+(cd packages/react-instantsearch && yarn build)
 yarn test:size
