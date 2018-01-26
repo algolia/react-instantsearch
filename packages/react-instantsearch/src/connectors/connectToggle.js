e8fd52ec1b29cba4cf41898c80cce8931adf6208
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-01-26 17:32:56 +0100

    docs(Toggle): remove the note on the off value we dont support it

--- packages/react-instantsearch/src/connectors/connectToggle.js
+++ packages/react-instantsearch/src/connectors/connectToggle.js
@@ -42,7 +42,7 @@ function cleanUp(props, searchState, context) {
 
 /**
  * connectToggle connector provides the logic to build a widget that will
- *  provides an on/off filtering feature based on an attribute value. Note that if you provide an “off” option, it will be refined at initialization.
+ * provides an on/off filtering feature based on an attribute value.
  * @name connectToggle
  * @kind connector
  * @requirements To use this widget, you'll need an attribute to toggle on.
