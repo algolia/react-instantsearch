c4a6b84b0f2571b7cfddbc588c54d6840d02d89c
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-03-07 14:29:25 +0100

    docs(RatingMenu): update usage

--- packages/react-instantsearch/src/widgets/RatingMenu.js
+++ packages/react-instantsearch/src/widgets/RatingMenu.js
@@ -30,20 +30,17 @@ import RatingMenu from '../components/RatingMenu';
  * @translationKey ratingLabel - Label value for the rating link
  * @example
  * import React from 'react';
+ * import { InstantSearch, RatingMenu } from 'react-instantsearch/dom';
  *
- * import { RatingMenu, InstantSearch } from 'react-instantsearch/dom';
- *
- * export default function App() {
- *   return (
- *     <InstantSearch
- *       appId="latency"
- *       apiKey="6be0576ff61c053d5f9a3225e2a90f76"
- *       indexName="ikea"
- *     >
- *       <RatingMenu attribute="rating" />
- *     </InstantSearch>
- *   );
- * }
+ * const App = () => (
+ *   <InstantSearch
+ *     appId="latency"
+ *     apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+ *     indexName="ikea"
+ *   >
+ *     <RatingMenu attribute="rating" />
+ *   </InstantSearch>
+ * );
  */
 
 const RatingMenuWidget = props => (
