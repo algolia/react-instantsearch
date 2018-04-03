20ef6850a14318f2abd9f0f900e366682b9c6832
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-03 16:35:25 +0200

    fix(connectGeoSearch): avoid outdated state

--- packages/react-instantsearch/src/connectors/connectGeoSearch.js
+++ packages/react-instantsearch/src/connectors/connectGeoSearch.js
@@ -29,16 +29,29 @@ export default createConnector({
     hasMapMoveSinceLastRefine: false,
   }),
 
-  getProvidedProps({ searchState, searchResults, setUiState }) {
+  getProvidedProps({ searchResults, searchState, setUiState }) {
     // @TODO: Handle multi-index
     const results = searchResults.results;
+    const searchParameters = results && results._state;
+    const isRefinedWithMapFromSearchState = Boolean(searchState[getId()]);
+    const isRefinedWithMapFromSearchParameters =
+      searchParameters && Boolean(searchParameters.insideBoundingBox);
+    const isRefinedWithMap =
+      // We read it from both becuase the SearchParameters & the searchState are not always
+      // in sync. When we set the refinement the searchState is used when we clear the refinement
+      // the SearchParameters is used. In the first case when we render the results are not there
+      // so we can't find the value from the results. The most up to date value is the searchState.
+      // But whren we clear the refinement the searchState is immediatly clear even when the items
+      // retrieve are still the one from the previous query with the bounding box. It leads to some
+      // issue with fitBounds.
+      isRefinedWithMapFromSearchState || isRefinedWithMapFromSearchParameters;
 
     return {
       // position: null // fetch from somewhere
       items: results ? results.hits.filter(_ => Boolean(_._geoloc)) : [],
-      isRefinedWithMap: Boolean(searchState[getId()]),
       toggleRefineOnMapMove: toggleRefineOnMapMove(setUiState),
       setMapMoveSinceLastRefine: setMapMoveSinceLastRefine(setUiState),
+      isRefinedWithMap,
     };
   },
 
