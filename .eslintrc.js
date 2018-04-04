09f0bb9daba2ba75f665135eefac99509389b13b
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-04 12:15:32 +0200

    chore(eslint): increase max-params limit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['algolia/react', 'algolia/jest'],
   rules: {
+    'max-params': ['error', 7],
     'no-param-reassign': 'off',
     'import/no-extraneous-dependencies': [
       'error',
