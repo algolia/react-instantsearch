982d0ef7b64d2bef282301cee947c83e096bdf78
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-17 10:13:05 +0200

    feat(connectGeoSearch): add getProvidedProps

---
+++ packages/react-instantsearch-core/src/connectors/__tests__/connectGeoSearch.js
@@ -0,0 +1,303 @@
+import { SearchResults, SearchParameters } from 'algoliasearch-helper';
+import connector from '../connectGeoSearch';
+
+jest.mock('../../core/createConnector', () => x => x);
+
+describe('connectGeoSearch', () => {
+  const empty = {};
+
+  const createSingleIndexInstance = () => ({
+    context: {
+      ais: {
+        mainTargetedIndex: 'index',
+      },
+    },
+  });
+
+  const createSearchResults = (hits = [], state) => ({
+    results: new SearchResults(new SearchParameters(state), [
+      {
+        hits,
+      },
+    ]),
+  });
+
+  describe('getProvidedProps', () => {
+    it('expect to return default provided props', () => {
+      const instance = createSingleIndexInstance();
+      const props = {};
+      const searchState = {};
+      const searchResults = empty;
+
+      const actual = connector.getProvidedProps.call(
+        instance,
+        props,
+        searchState,
+        searchResults
+      );
+
+      const expectation = {
+        hits: [],
+        position: null,
+        currentRefinement: null,
+        isRefinedWithMap: false,
+      };
+
+      expect(actual).toEqual(expectation);
+    });
+
+    describe('hits', () => {
+      it('expect to return hits when we have results', () => {
+        const hits = [
+          { objectID: '123', _geoloc: true },
+          { objectID: '456', _geoloc: true },
+          { objectID: '789', _geoloc: true },
+        ];
+
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchState = {};
+        const searchResults = createSearchResults(hits);
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        const expectation = [
+          { objectID: '123', _geoloc: true },
+          { objectID: '456', _geoloc: true },
+          { objectID: '789', _geoloc: true },
+        ];
+
+        expect(actual.hits).toEqual(expectation);
+      });
+
+      it('expect to return hits with only "_geoloc" when we have results', () => {
+        const hits = [
+          { objectID: '123', _geoloc: true },
+          { objectID: '456', _geoloc: false },
+          { objectID: '789', _geoloc: true },
+        ];
+
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchState = {};
+        const searchResults = createSearchResults(hits);
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        const expectation = [
+          { objectID: '123', _geoloc: true },
+          { objectID: '789', _geoloc: true },
+        ];
+
+        expect(actual.hits).toEqual(expectation);
+      });
+
+      it("expect to return empty hits when we don't have results", () => {
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchState = {};
+        const searchResults = empty;
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        const expectation = [];
+
+        expect(actual.hits).toEqual(expectation);
+      });
+    });
+
+    describe('position', () => {
+      it('expect to return the position from the searchState', () => {
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchResults = createSearchResults();
+        const searchState = {
+          aroundLatLng: {
+            lat: 10,
+            lng: 12,
+          },
+        };
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        expect(actual.position).toEqual({
+          lat: 10,
+          lng: 12,
+        });
+      });
+
+      it('expect to return null from an empty searchState', () => {
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchState = {};
+        const searchResults = createSearchResults();
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        expect(actual.position).toBe(null);
+      });
+    });
+
+    describe('currentRefinement', () => {
+      it('expect to return the boundingBox from the searchState', () => {
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchResults = createSearchResults();
+        const searchState = {
+          boundingBox: {
+            northEast: {
+              lat: 10,
+              lng: 12,
+            },
+            southWest: {
+              lat: 12,
+              lng: 14,
+            },
+          },
+        };
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        expect(actual.currentRefinement).toEqual({
+          northEast: {
+            lat: 10,
+            lng: 12,
+          },
+          southWest: {
+            lat: 12,
+            lng: 14,
+          },
+        });
+      });
+
+      it('expect to return an null from an empty searchState', () => {
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchState = {};
+        const searchResults = createSearchResults();
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        expect(actual.currentRefinement).toBe(null);
+      });
+    });
+
+    describe('isRefinedWithMap', () => {
+      it("expect to return true when it's refined with the map (from the searchState)", () => {
+        const hits = [
+          { objectID: '123', _geoloc: true },
+          { objectID: '456', _geoloc: true },
+          { objectID: '789', _geoloc: true },
+        ];
+
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchResults = createSearchResults(hits);
+        const searchState = {
+          boundingBox: {
+            northEast: {
+              lat: 10,
+              lng: 12,
+            },
+            southWest: {
+              lat: 12,
+              lng: 14,
+            },
+          },
+        };
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        expect(actual.isRefinedWithMap).toBe(true);
+      });
+
+      it("expect to return true when it's refined with the map (from the searchParameters)", () => {
+        const sliceSearchParameters = {
+          insideBoundingBox: '10, 12, 12, 14',
+        };
+
+        const hits = [
+          { objectID: '123', _geoloc: true },
+          { objectID: '456', _geoloc: true },
+          { objectID: '789', _geoloc: true },
+        ];
+
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchState = {};
+        const searchResults = createSearchResults(hits, sliceSearchParameters);
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        expect(actual.isRefinedWithMap).toBe(true);
+      });
+
+      it("expect to return false when it's not refined with the map", () => {
+        const hits = [
+          { objectID: '123', _geoloc: true },
+          { objectID: '456', _geoloc: true },
+          { objectID: '789', _geoloc: true },
+        ];
+
+        const instance = createSingleIndexInstance();
+        const props = {};
+        const searchState = {};
+        const searchResults = createSearchResults(hits);
+
+        const actual = connector.getProvidedProps.call(
+          instance,
+          props,
+          searchState,
+          searchResults
+        );
+
+        expect(actual.isRefinedWithMap).toBe(false);
+      });
+    });
+  });
+});
---
+++ packages/react-instantsearch-core/src/connectors/connectGeoSearch.js
@@ -0,0 +1,63 @@
+import createConnector from '../core/createConnector';
+import { getResults, getCurrentRefinementValue } from '../core/indexUtils';
+
+const getBoundingBoxId = () => 'boundingBox';
+
+// To control the map with an external widget the other widget
+// must write the value in the attribute `aroundLatLng`
+const getAroundLatLngId = () => 'aroundLatLng';
+
+const getCurrentRefinement = (props, searchState, context) =>
+  getCurrentRefinementValue(
+    props,
+    searchState,
+    context,
+    getBoundingBoxId(),
+    null,
+    x => x
+  );
+
+const getCurrentPosition = (props, searchState, context) =>
+  getCurrentRefinementValue(
+    props,
+    searchState,
+    context,
+    getAroundLatLngId(),
+    null,
+    x => x
+  );
+
+export default createConnector({
+  displayName: 'AlgoliaGeoSearch',
+
+  getProvidedProps(props, searchState, searchResults) {
+    const results = getResults(searchResults, this.context);
+    const currentRefinement = getCurrentRefinement(
+      props,
+      searchState,
+      this.context
+    );
+
+    const isRefinedWithMapFromSearchState = Boolean(currentRefinement);
+    const isRefinedWithMapFromSearchParameters = Boolean(
+      results && results._state.insideBoundingBox
+    );
+
+    const isRefinedWithMap =
+      // We read it from both becuase the SearchParameters & the searchState are not always
+      // in sync. When we set the refinement the searchState is used when we clear the refinement
+      // the SearchParameters is used. In the first case when we render the results are not there
+      // so we can't find the value from the results. The most up to date value is the searchState.
+      // But whren we clear the refinement the searchState is immediatly clear even when the items
+      // retrieve are still the one from the previous query with the bounding box. It leads to some
+      // issue with fitBounds.
+      isRefinedWithMapFromSearchState || isRefinedWithMapFromSearchParameters;
+
+    return {
+      hits: !results ? [] : results.hits.filter(_ => Boolean(_._geoloc)),
+      position: getCurrentPosition(props, searchState, this.context),
+      currentRefinement,
+      isRefinedWithMap,
+    };
+  },
+});
