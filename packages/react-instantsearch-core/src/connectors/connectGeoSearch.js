99daa9aee024ec26feb8b794927dc590a36ca16e
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-18 11:55:12 +0200

    docs(connectGeoSearch): add documentation

--- packages/react-instantsearch-core/src/connectors/connectGeoSearch.js
+++ packages/react-instantsearch-core/src/connectors/connectGeoSearch.js
@@ -7,6 +7,23 @@ import {
   cleanUpValue,
 } from '../core/indexUtils';
 
+/**
+ * The GeoSearch connector provides the logic to build a widget that will display the results on a map.
+ * It also provides a way to search for results based on their position. The connector provides function to manage the search experience (search on map interaction).
+ * @name connectGeoSearch
+ * @kind connector
+ * @requirements Note that the GeoSearch connector uses the [geosearch](https://www.algolia.com/doc/guides/searching/geo-search) capabilities of Algolia.
+ * Your hits **must** have a `_geoloc` attribute in order to be passed to the rendering function.
+ * Currently, the feature is not compatible with multiple values in the `_geoloc` attribute.
+ * @propType {{ northEast: { lat: number, lng: number }, southWest: { lat: number, lng: number } }} [defaultRefinement] - Default search state of the widget containing the bounds for the map
+ * @providedPropType {function({ northEast: { lat: number, lng: number }, southWest: { lat: number, lng: number } })} refine - a function to toggle the refinement
+ * @providedPropType {function} createURL - a function to generate a URL for the corresponding search state
+ * @providedPropType {array.<object>} hits - the records that matched the search
+ * @providedPropType {boolean} isRefinedWithMap - true if the current refinement is set with the map bounds
+ * @providedPropType {{ northEast: { lat: number, lng: number }, southWest: { lat: number, lng: number } }} [currentRefinement] - the refinement currently applied
+ * @providedPropType {{ lat: number, lng: number }} [position] - the position of the search
+ */
+
 // To control the map with an external widget the other widget
 // **must** write the value in the attribute `aroundLatLng`
 const getBoundingBoxId = () => 'boundingBox';
