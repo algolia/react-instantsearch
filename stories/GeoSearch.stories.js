e95010cd6e29feef63fa87665b9108a65866ac7d
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-24 18:54:27 +0200

    feat(GeoSearch): add default & initial stories

--- stories/GeoSearch.stories.js
+++ stories/GeoSearch.stories.js
@@ -1,20 +1,79 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { setAddon, storiesOf } from '@storybook/react';
 import JSXAddon from 'storybook-addon-jsx';
-import { GoogleMapsLoader } from '../packages/react-instantsearch-dom-geo/src/index';
+import {
+  GoogleMapsLoader,
+  GeoSearch,
+} from '../packages/react-instantsearch-dom-geo/src/index';
 import { displayName, filterProps, WrapWithHits } from './util';
+import { Configure } from '../packages/react-instantsearch/dom';
 
 setAddon(JSXAddon);
 
 const stories = storiesOf('GeoSearch', module);
 
+const Container = ({ children }) => (
+  <div style={{ height: 500 }}>{children}</div>
+);
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const initialZoom = 12;
+const initialPosition = {
+  lat: 40.71,
+  lng: -74.01,
+};
+
 stories.addWithJSX(
   'default',
   () => (
-    <WrapWithHits linkedStoryGroup="GeoSearch">
-      <GoogleMapsLoader apiKey="AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8">
-        {google => console.log(google) || null}
-      </GoogleMapsLoader>
+    <WrapWithHits
+      indexName="airbnb"
+      linkedStoryGroup="GeoSearch"
+      searchParameters={{ hitsPerPage: 20 }}
+    >
+      <Configure aroundLatLngViaIP />
+
+      <Container>
+        <GoogleMapsLoader apiKey="AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8">
+          {google => <GeoSearch google={google}>{() => null}</GeoSearch>}
+        </GoogleMapsLoader>
+      </Container>
+    </WrapWithHits>
+  ),
+  {
+    displayName,
+    filterProps,
+  }
+);
+
+// Only UI
+stories.addWithJSX(
+  'with zoom & center',
+  () => (
+    <WrapWithHits
+      indexName="airbnb"
+      linkedStoryGroup="GeoSearch"
+      searchParameters={{ hitsPerPage: 20 }}
+    >
+      <Configure aroundLatLngViaIP />
+
+      <Container>
+        <GoogleMapsLoader apiKey="AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8">
+          {google => (
+            <GeoSearch
+              google={google}
+              initialZoom={initialZoom}
+              initialPosition={initialPosition}
+            >
+              {() => null}
+            </GeoSearch>
+          )}
+        </GoogleMapsLoader>
+      </Container>
     </WrapWithHits>
   ),
   {
