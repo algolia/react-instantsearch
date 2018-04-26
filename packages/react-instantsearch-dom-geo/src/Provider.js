caae7741e52fe5570849a571fddba00f07db5253
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-26 10:20:29 +0200

    feat(Provider): handle hasMapMoveSinceLastRefine

--- packages/react-instantsearch-dom-geo/src/Provider.js
+++ packages/react-instantsearch-dom-geo/src/Provider.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connectGeoSearch } from 'react-instantsearch-dom';
 import { LatLngPropType, BoundingBoxPropType } from './propTypes';
 
+export const STATE_CONTEXT = '__ais_geo_search__state__';
+
 export class Provider extends Component {
   static propTypes = {
     hits: PropTypes.arrayOf(PropTypes.object).isRequired,
@@ -11,6 +13,40 @@ export class Provider extends Component {
     children: PropTypes.func.isRequired,
     position: LatLngPropType,
     currentRefinement: BoundingBoxPropType,
+  };
+
+  static childContextTypes = {
+    [STATE_CONTEXT]: PropTypes.shape({
+      hasMapMoveSinceLastRefine: PropTypes.bool.isRequired,
+      setMapMoveSinceLastRefine: PropTypes.func.isRequired,
+    }).isRequired,
+  };
+
+  state = {
+    hasMapMoveSinceLastRefine: false,
+  };
+
+  getChildContext() {
+    const { hasMapMoveSinceLastRefine } = this.state;
+
+    return {
+      [STATE_CONTEXT]: {
+        setMapMoveSinceLastRefine: this.setMapMoveSinceLastRefine,
+        hasMapMoveSinceLastRefine,
+      },
+    };
+  }
+
+  setMapMoveSinceLastRefine = next => {
+    const { hasMapMoveSinceLastRefine } = this.state;
+
+    if (hasMapMoveSinceLastRefine === next) {
+      return;
+    }
+
+    this.setState(() => ({
+      hasMapMoveSinceLastRefine: next,
+    }));
   };
 
   render() {
@@ -23,9 +59,13 @@ export class Provider extends Component {
       children,
     } = this.props;
 
+    const { hasMapMoveSinceLastRefine } = this.state;
+
     return children({
+      setMapMoveSinceLastRefine: this.setMapMoveSinceLastRefine,
       hits,
       isRefinedWithMap,
+      hasMapMoveSinceLastRefine,
       position,
       currentRefinement,
       refine,
--- packages/react-instantsearch-dom-geo/src/__tests__/Provider.js
+++ packages/react-instantsearch-dom-geo/src/__tests__/Provider.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Provider } from '../Provider';
+import { Provider, STATE_CONTEXT } from '../Provider';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -13,6 +13,8 @@ describe('Provider', () => {
     isRefinedWithMap: false,
     refine: () => {},
   };
+
+  const lastRenderArgs = fn => fn.mock.calls[fn.mock.calls.length - 1][0];
 
   it('expect to call children with props', () => {
     const children = jest.fn(x => x);
@@ -29,7 +31,79 @@ describe('Provider', () => {
       position: null,
       currentRefinement: null,
       isRefinedWithMap: false,
+      hasMapMoveSinceLastRefine: false,
+      setMapMoveSinceLastRefine: expect.any(Function),
       refine: expect.any(Function),
+    });
+  });
+
+  describe('setMapMoveSinceLastRefine', () => {
+    it('expect to update the state with the given value', () => {
+      const children = jest.fn(x => x);
+
+      const props = {
+        ...defaultProps,
+      };
+
+      const wrapper = shallow(<Provider {...props}>{children}</Provider>);
+
+      lastRenderArgs(children).setMapMoveSinceLastRefine(true);
+
+      expect(wrapper.state().hasMapMoveSinceLastRefine).toBe(true);
+    });
+
+    it('expect to only update the state when the given is different', () => {
+      const children = jest.fn(x => x);
+
+      const props = {
+        ...defaultProps,
+      };
+
+      shallow(<Provider {...props}>{children}</Provider>);
+
+      expect(children).toHaveBeenCalledTimes(1);
+      expect(children).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          hasMapMoveSinceLastRefine: false,
+        })
+      );
+
+      lastRenderArgs(children).setMapMoveSinceLastRefine(true);
+
+      expect(children).toHaveBeenCalledTimes(2);
+      expect(children).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          hasMapMoveSinceLastRefine: true,
+        })
+      );
+
+      lastRenderArgs(children).setMapMoveSinceLastRefine(true);
+
+      expect(children).toHaveBeenCalledTimes(2);
+      expect(children).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          hasMapMoveSinceLastRefine: true,
+        })
+      );
+    });
+  });
+
+  describe('context', () => {
+    it('expect to expose hasMapMoveSinceLastRefine & setMapMoveSinceLastRefine', () => {
+      const children = jest.fn(x => x);
+
+      const props = {
+        ...defaultProps,
+      };
+
+      const wrapper = shallow(<Provider {...props}>{children}</Provider>);
+
+      expect(wrapper.instance().getChildContext()).toEqual({
+        [STATE_CONTEXT]: expect.objectContaining({
+          hasMapMoveSinceLastRefine: false,
+          setMapMoveSinceLastRefine: expect.any(Function),
+        }),
+      });
     });
   });
 });
