5d9683a3a6c435ac9f1dfb6067d976592fe450ae
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-05-18 17:24:35 +0200

    fix(legacy): remove Babel on src for the build

--- packages/react-instantsearch/scripts/build.sh
+++ packages/react-instantsearch/scripts/build.sh
@@ -15,7 +15,6 @@ NODE_ENV=es babel -q dom.js -o dist/es/dom.js &&
 NODE_ENV=es babel -q connectors.js -o dist/es/connectors.js &&
 NODE_ENV=es babel -q native.js -o dist/es/native.js &&
 NODE_ENV=es babel -q server.js -o dist/es/server.js &&
-NODE_ENV=es babel -q --ignore test.js,__mocks__ --out-dir dist/es/src src
 
 # then also make a commonjs build
 babel -q index.js -o dist/index.js &&
@@ -23,7 +22,6 @@ babel -q dom.js -o dist/dom.js &&
 babel -q connectors.js -o dist/connectors.js &&
 babel -q native.js -o dist/native.js &&
 babel -q server.js -o dist/server.js &&
-babel -q --ignore test.js,__mocks__ --out-dir dist/src src
 
 # finally a UMD build
 BABEL_ENV=es rollup -c rollup.config.js
