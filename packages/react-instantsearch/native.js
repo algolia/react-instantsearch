fa8d5a71108d19212c436a4e8e5ea4798bb59e49
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-05-18 18:47:02 +0200

    chore: move native logic to native package

--- packages/react-instantsearch/native.js
+++ packages/react-instantsearch/native.js
@@ -1,13 +1,3 @@
-import algoliasearch from 'algoliasearch/reactnative';
-import { createInstantSearch, createIndex } from 'react-instantsearch-core';
-import { View } from 'react-native';
-
-export const InstantSearch = createInstantSearch(algoliasearch, {
-  Root: View,
-});
-
-export const Index = createIndex({
-  Root: View,
-});
-
-export { Configure } from 'react-instantsearch-core';
+export { InstantSearch } from 'react-instantsearch-native';
+export { Index } from 'react-instantsearch-native';
+export { Configure } from 'react-instantsearch-native';
