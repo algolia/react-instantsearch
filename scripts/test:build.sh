99e4f782316be6f8bc98450f64417adebaad05b5
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-05-18 16:06:35 +0200

    chore: build dependant packages before main

--- scripts/test:build.sh
+++ scripts/test:build.sh
@@ -5,5 +5,8 @@
 
 set -e # exit when error
 
+# Will be removed in upcoming PR
+lerna run build --scope react-instantsearch-*
+
 (cd packages/react-instantsearch && yarn build)
 (cd packages/react-instantsearch && yarn test:size)
