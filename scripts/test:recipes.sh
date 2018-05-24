56a5b66f5123db5e0f204569979bb740ffef93ce
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-05-24 12:21:33 +0200

    chore(recipes): update test script

--- scripts/test:recipes.sh
+++ scripts/test:recipes.sh
@@ -5,10 +5,11 @@
 # cause test to fail if one fails
 set -e
 
-for d in examples/* ; do
-    cd $d
+for example in examples/* ; do
+  (
+    cd $example
     yarn
     yarn build
     yarn test
-    cd ../..
+  )
 done
