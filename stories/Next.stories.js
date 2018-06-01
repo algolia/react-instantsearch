85802d05cc956bbabd082f9ba52a643756c68809
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-01 16:43:25 +0200

    feat(stories): update next stories

--- stories/Next.stories.js
+++ stories/Next.stories.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Hello } from 'react-instantsearch-next';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  RefinementList,
+  SearchBox,
+} from 'react-instantsearch-next';
 
 const stories = storiesOf('<Next>', module);
+const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
 
-stories.add('default', () => <Hello />);
+stories.add('default', () => (
+  <InstantSearch searchClient={client} indexName="ikea">
+    <SearchBox />
+    <RefinementList attribute="category" />
+    <Hits />
+  </InstantSearch>
+));
