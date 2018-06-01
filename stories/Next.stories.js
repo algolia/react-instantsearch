e263825e18f54a14db3e7c06ef146c7ef29aec5c
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-01 17:35:25 +0200

    chore(stories): use instant_search index with next

--- stories/Next.stories.js
+++ stories/Next.stories.js
@@ -12,9 +12,9 @@ const stories = storiesOf('<Next>', module);
 const client = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
 
 stories.add('default', () => (
-  <InstantSearch searchClient={client} indexName="ikea">
+  <InstantSearch searchClient={client} indexName="instant_search">
     <SearchBox />
-    <RefinementList attribute="category" />
+    <RefinementList attribute="categories" />
     <Hits />
   </InstantSearch>
 ));
