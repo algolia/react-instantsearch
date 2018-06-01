dc588dbaef53cbd25c4b9a55d91731564428d67a
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-01 16:37:05 +0200

    feat(InstantSearch): add component

---
+++ packages/react-instantsearch-next/src/InstantSearch.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import createInstantSearch from 'instantsearch.js/es';
+import Context from './Context';
+
+class InstantSearch extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    searchClient: PropTypes.shape({
+      search: PropTypes.func.isRequired,
+      searchForFacetValues: PropTypes.func.isRequired,
+    }).isRequired,
+    indexName: PropTypes.string.isRequired,
+  };
+
+  constructor(...args) {
+    super(...args);
+
+    this.instance = createInstantSearch({
+      searchClient: this.props.searchClient,
+      indexName: this.props.indexName,
+    });
+  }
+
+  componentDidMount() {
+    this.instance.start();
+  }
+
+  render() {
+    return (
+      // prettier-ignore
+      <Context.Provider value={this.instance}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export default InstantSearch;
