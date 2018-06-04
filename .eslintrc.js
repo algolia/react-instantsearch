66384bcf6560995cd28243286fa82132a4e2e687
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-04 13:23:08 +0200

    chore(eslint): allow unused with rest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ['algolia/react', 'algolia/jest'],
   rules: {
     'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
   },
 };
