1dd6a6ab91cf0ec675f9a6725ffbc9dac0e8927c
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-07 14:49:08 +0200

    refactor: re-order export

--- packages/react-instantsearch-next/src/index.js
+++ packages/react-instantsearch-next/src/index.js
@@ -5,12 +5,12 @@ export { default as createWidget } from './core/createWidget';
 export { default as InstantSearch } from './core/InstantSearch';
 export { default as Highlight } from './Highlight';
 
-// DOM
-export { HitsWidget as Hits } from './Hits';
-export { RefinementListWidget as RefinementList } from './RefinementList';
-export { SearchBoxWidget as SearchBox } from './SearchBox';
-
 // Renderer
 export { HitsRenderer } from './Hits';
 export { RefinementListRenderer } from './RefinementList';
 export { SearchBoxRenderer } from './SearchBox';
+
+// DOM
+export { HitsWidget as Hits } from './Hits';
+export { RefinementListWidget as RefinementList } from './RefinementList';
+export { SearchBoxWidget as SearchBox } from './SearchBox';
