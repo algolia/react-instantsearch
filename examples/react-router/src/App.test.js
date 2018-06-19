0029e22d97471e097758e51fb8a05453c30c9f2c
Yotam <yotammanor@gmail.com>
2018-06-19 10:53:24 +0300

    use real location object in example app test
    The existing solution mocks a specific behaviour of the history.location object, but when trying to create tests that require manipulation of the browser location or history, that won't do.
    
    The suggested change shows how to create a real location object, that is usable in tests. 
    See example usage in our react-instantsearch based app, https://github.com/masschallenge/directory/blob/development/src/components/__tests__/AlgoliaSearch.test.js

--- examples/react-router/src/App.test.js
+++ examples/react-router/src/App.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
+import {createMemoryHistory} from 'history'
+
+const history = createMemoryHistory('/')
+const { location } = history
 
 describe('react-router recipe', () => {
   it('App renders without crashing', () => {
     const component = renderer.create(
-      <App location={{ search: { slice: jest.fn() } }} />
+      <App location={location} />
     );
 
     expect(component.toJSON()).toMatchSnapshot();
