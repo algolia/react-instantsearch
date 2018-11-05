a6d81f872423aba5c0fb4457346f434db8dc3468
fitimvata <fitimvata28@gmail.com>
2018-11-05 21:42:09 +0100

    update heat map when props change

--- packages/react-instantsearch-dom-maps/src/HeatmapLayer.js
+++ packages/react-instantsearch-dom-maps/src/HeatmapLayer.js
@@ -4,7 +4,7 @@ import { createFilterProps } from './utils';
 import { GeolocHitsPropType } from './propTypes';
 import { GOOGLE_MAPS_CONTEXT } from './GoogleMaps';
 
-const excludes = ['children'];
+const excludes = ['children', 'hits'];
 const filterProps = createFilterProps(excludes);
 
 function hitsToLatLngData(hits = [], google) {
@@ -14,9 +14,63 @@ function hitsToLatLngData(hits = [], google) {
   });
 }
 
+function arrayEqual(hits = [], otherHits = [], callback = () => {}) {
+  if (hits.length !== otherHits.length) {
+    return false;
+  }
+
+  for (let index = 0; index < hits.length; index++) {
+    const hit = hits[index];
+    const otherHit = otherHits[index];
+    if (!callback(hit, otherHit)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function hitsAreEqual(hits = [], otherHits = []) {
+  return arrayEqual(
+    hits,
+    otherHits,
+    (hit, otherHit) => hit.objectID === otherHit.objectID
+  );
+}
+
+const equal = (value, otherValue) => value === otherValue;
+
+const heatMapOptions = [
+  {
+    key: 'dissipating',
+    equal,
+  },
+  {
+    key: 'maxIntensity',
+    equal,
+  },
+  {
+    key: 'opacity',
+    equal,
+  },
+  {
+    key: 'radius',
+    equal,
+  },
+  {
+    key: 'gradient',
+    equal: (array1, array2) => arrayEqual(array1, array2, equal),
+  },
+];
+
 class HeatmapLayer extends Component {
   static propTypes = {
     hits: GeolocHitsPropType.isRequired,
+    dissipating: PropTypes.bool,
+    gradient: PropTypes.arrayOf(PropTypes.string),
+    maxIntensity: PropTypes.number,
+    opacity: PropTypes.number,
+    radius: PropTypes.number,
   };
 
   static contextTypes = {
@@ -35,6 +89,31 @@ class HeatmapLayer extends Component {
       map: instance,
       data: hitsToLatLngData(hits, google),
     });
+  }
+
+  componentDidUpdate() {
+    const { hits, ...props } = this.props;
+
+    const { google } = this.context[GOOGLE_MAPS_CONTEXT];
+
+    this.instance.setData(hitsToLatLngData(hits, google));
+    this.instance.setOptions(filterProps(props));
+  }
+
+  shouldComponentUpdate(nexProps) {
+    const { hits } = this.props;
+    if (!hitsAreEqual(hits, nexProps.hits)) {
+      return true;
+    }
+
+    const options = filterProps(nexProps);
+
+    const hasChange = heatMapOptions.reduce(
+      (acc, value) =>
+        value.equal(options[value.key], this.props[value.key]) || acc,
+      false
+    );
+    return hasChange;
   }
 
   componentWillUnmount() {
