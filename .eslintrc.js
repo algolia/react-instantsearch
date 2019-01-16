754d20350aa68465865d2f036433f96b4de140b9
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-01-16 16:53:33 +0100

    chore(eslint): disable extranous dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,10 @@ module.exports = {
     'no-param-reassign': 'off',
     // @TODO: to remove once `eslint-config-algolia` ships the change
     'valid-jsdoc': 'off',
+    // @TODO: re-enable once the rule is properly setup for monorepos
+    // https://github.com/benmosher/eslint-plugin-import/issues/1103
+    // https://github.com/benmosher/eslint-plugin-import/issues/1174
+    'import/no-extraneous-dependencies': 'off',
   },
   settings: {
     react: {
