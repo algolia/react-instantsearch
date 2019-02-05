5048f8b5d483d773d50ed800e7cf808812830d42
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-02-05 09:46:01 +0100

    chore(Jest): use roots rather than prefix testMatch

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,12 @@ module.exports = {
   // @TODO: Jest supports TypeScript by default in the next major (24.x.x). We
   // can remove the specific configuration once it's released.
   // https://github.com/facebook/jest/pull/7533
+  roots: ['<rootDir>/packages'],
   moduleFileExtensions: ['js', 'ts', 'tsx'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testMatch: [
-    '<rootDir>/packages/**/__tests__/**/*.(js|ts|tsx)',
-    '<rootDir>/packages/**/?(*.)+(spec|test).(js|ts|tsx)',
+    '**/__tests__/**/*.(js|ts|tsx)',
+    '**/?(*.)+(spec|test).(js|ts|tsx)',
   ],
   transform: {
     '^.+\\.(js|ts|tsx)$': 'babel-jest',
