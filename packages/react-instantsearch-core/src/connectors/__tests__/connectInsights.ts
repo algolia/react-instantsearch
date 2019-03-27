2eb25d4c19a61f27131e9f77dd6976cd78391bbd
Youcef Mammar <youcef.mammar@algolia.com>
2019-03-27 12:59:33 +0100

    feedback: simplified tests

--- packages/react-instantsearch-core/src/connectors/__tests__/connectInsights.ts
+++ packages/react-instantsearch-core/src/connectors/__tests__/connectInsights.ts
@@ -2,126 +2,87 @@ import connect from '../connectInsights';
 
 jest.mock('../../core/createConnector', () => x => x);
 
-describe('connectInsights', () => {
-  let insightsClient;
-  let props;
+function setup() {
+  const insightsClient = jest.fn();
 
-  beforeEach(() => {
-    insightsClient = jest.fn();
-
-    const createMultiIndexContext = () => ({
-      context: {
-        ais: {
-          mainTargetedIndex: 'theFirstIndex',
-        },
-        multiIndexContext: {
-          targetedIndex: 'theIndex',
-        },
+  const createMultiIndexContext = () => ({
+    context: {
+      ais: {
+        mainTargetedIndex: 'theFirstIndex',
       },
-    });
-
-    const context = createMultiIndexContext();
-    const getProvidedProps = connect(insightsClient).getProvidedProps.bind(
-      context
-    );
-
-    const hit = {
-      objectID: 'objectID_42',
-      __position: 42,
-      __queryID: 'theQueryID',
-    };
-    const searchResults = { results: { theIndex: { index: 'theIndex' } } };
-    props = getProvidedProps({ hit }, null, searchResults);
+      multiIndexContext: {
+        targetedIndex: 'theIndex',
+      },
+    },
   });
 
+  const context = createMultiIndexContext();
+  const getProvidedProps = connect(insightsClient).getProvidedProps.bind(
+    context
+  );
+
+  const hit = {
+    objectID: 'objectID_42',
+    __position: 42,
+    __queryID: 'theQueryID',
+  };
+  const searchResults = { results: { theIndex: { index: 'theIndex' } } };
+  const props = getProvidedProps({ hit }, null, searchResults);
+  return { insightsClient, props };
+}
+
+describe('connectInsights', () => {
   it('should expose an `insights` property', () => {
+    const { props } = setup();
     expect(props).toHaveProperty('insights');
   });
 
   it('exposed `insights` should be a function', () => {
+    const { props } = setup();
     expect(typeof props.insights).toBe('function');
   });
 
   describe('when called with `clickedObjectIDsAfterSearch`', () => {
+    let insightsClient;
     beforeEach(() => {
+      const { insightsClient: aa, props } = setup();
+      insightsClient = aa;
       props.insights('clickedObjectIDsAfterSearch', {
         eventName: 'Add to cart',
       });
     });
 
-    it('should forward call to insightsClient', () => {
+    it('should forward call to insightsClient with the correct payload', () => {
       expect(insightsClient).toHaveBeenCalledTimes(1);
-    });
-
-    it('should pass method', () => {
-      const [method] = insightsClient.mock.calls[0];
+      const [method, payload] = insightsClient.mock.calls[0];
       expect(method).toEqual('clickedObjectIDsAfterSearch');
-    });
-
-    it('should pass eventName', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.eventName).toEqual('Add to cart');
-    });
-
-    it('should pass objectIDs', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.objectIDs).toEqual(['objectID_42']);
-    });
-
-    it('should pass positions', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.positions).toEqual([42]);
-    });
-
-    it('should pass queryID', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.queryID).toEqual('theQueryID');
-    });
-
-    it('should pass indexName', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.index).toEqual('theIndex');
     });
   });
 
   describe('when called with `convertedObjectIDsAfterSearch`', () => {
+    let insightsClient;
     beforeEach(() => {
+      const { insightsClient: aa, props } = setup();
+      insightsClient = aa;
       props.insights('convertedObjectIDsAfterSearch', {
         eventName: 'Add to cart',
       });
     });
 
-    it('should forward call to insightsClient', () => {
+    it('should forward call to insightsClient with the correct payload', () => {
       expect(insightsClient).toHaveBeenCalledTimes(1);
-    });
 
-    it('should pass method', () => {
-      const [method] = insightsClient.mock.calls[0];
+      const [method, payload] = insightsClient.mock.calls[0];
       expect(method).toEqual('convertedObjectIDsAfterSearch');
-    });
-
-    it('should pass eventName', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.eventName).toEqual('Add to cart');
-    });
-
-    it('should pass objectIDs', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.objectIDs).toEqual(['objectID_42']);
-    });
-
-    it('should not pass positions', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload).not.toHaveProperty('positions');
-    });
-
-    it('should pass queryID', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.queryID).toEqual('theQueryID');
-    });
-
-    it('should pass indexName', () => {
-      const [, payload] = insightsClient.mock.calls[0];
       expect(payload.index).toEqual('theIndex');
     });
   });
@@ -129,6 +90,7 @@ describe('connectInsights', () => {
   describe('when called with an unsupported method', () => {
     it('should reject the call', () => {
       expect(() => {
+        const { props } = setup();
         // @ts-ignore
         props.insights('wrong-method-name', {
           eventName: 'Add to cart',
