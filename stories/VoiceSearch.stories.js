e1f73fa49e82a9794877539f5c5e4cfbcf193edd
Eunjae Lee <eunjae.lee@algolia.com>
2019-04-15 17:59:04 +0200

    test(voiceSearch): add stories

--- stories/VoiceSearch.stories.js
+++ stories/VoiceSearch.stories.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { VoiceSearch } from 'react-instantsearch-dom';
+import { VoiceSearch, SearchBox } from 'react-instantsearch-dom';
 import { WrapWithHits } from './util';
 
 const stories = storiesOf('VoiceSearch', module);
 
-stories.add('default', () => (
-  <WrapWithHits
-    searchBox={false}
-    hasPlayground={true}
-    linkedStoryGroup="VoiceSearch"
-  >
-    <VoiceSearch />
-  </WrapWithHits>
-));
+stories
+  .add('default', () => (
+    <WrapWithHits
+      searchBox={false}
+      hasPlayground={true}
+      linkedStoryGroup="VoiceSearch"
+    >
+      <p style={{ color: '#999', fontStyle: 'italic' }}>
+        To see this button disabled, test it on unsupported browsers like
+        Safari, Firefox, etc.
+      </p>
+      <VoiceSearch />
+    </WrapWithHits>
+  ))
+  .add('without status', () => (
+    <WrapWithHits
+      searchBox={false}
+      hasPlayground={true}
+      linkedStoryGroup="VoiceSearch"
+    >
+      <VoiceSearch statusComponent={() => null} />
+    </WrapWithHits>
+  ))
+  .add('with a SearchBox', () => (
+    <WrapWithHits
+      searchBox={false}
+      hasPlayground={true}
+      linkedStoryGroup="VoiceSearch"
+    >
+      <VoiceSearch />
+      <SearchBox />
+    </WrapWithHits>
+  ))
+  .add('with a custom button text', () => (
+    <WrapWithHits
+      searchBox={false}
+      hasPlayground={true}
+      linkedStoryGroup="VoiceSearch"
+    >
+      <VoiceSearch
+        buttonComponent={({ isListening }) => (isListening ? 'Stop' : 'Start')}
+      />
+    </WrapWithHits>
+  ));
