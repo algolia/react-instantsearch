4481bd459956585fbba0b29f09c80a5345cf0ab4
Eunjae Lee <eunjae.lee@algolia.com>
2019-04-16 15:57:53 +0200

    chore(voiceSearch): clean up svg

--- packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
+++ packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
@@ -35,73 +35,56 @@ type VoiceSearchProps = {
   statusComponent?: React.FC<InnerComponentProps>;
 };
 
-const DefaultButton: React.FC<InnerComponentProps> = ({
-  status,
-  errorCode,
-  isListening,
-}) => {
+const getDefaultButtonInnerElement = (
+  status: string,
+  errorCode: string | undefined,
+  isListening: boolean
+) => {
   if (status === 'error' && errorCode === 'not-allowed') {
     return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
+      <>
         <line x1="1" y1="1" x2="23" y2="23" />
         <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />
         <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />
         <line x1="12" y1="19" x2="12" y2="23" />
         <line x1="8" y1="23" x2="16" y2="23" />
-      </svg>
+      </>
     );
-  } else if (isListening) {
+  } else {
     return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
+      <>
         <path
           d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"
-          fill="currentColor"
+          fill={isListening ? 'currentColor' : ''}
         />
         <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
         <line x1="12" y1="19" x2="12" y2="23" />
         <line x1="8" y1="23" x2="16" y2="23" />
-      </svg>
-    );
-  } else {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />
-        <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
-        <line x1="12" y1="19" x2="12" y2="23" />
-        <line x1="8" y1="23" x2="16" y2="23" />
-      </svg>
+      </>
     );
   }
+};
+
+const DefaultButton: React.FC<InnerComponentProps> = ({
+  status,
+  errorCode,
+  isListening,
+}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      ${getDefaultButtonInnerElement(status, errorCode, isListening)}
+    </svg>
+  );
 };
 
 const DefaultStatus: React.FC<InnerComponentProps> = ({ status }) => (
