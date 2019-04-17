bb193c52d105d03e87e50af929ea8128b42bc16b
Eunjae Lee <eunjae.lee@algolia.com>
2019-04-17 15:39:30 +0200

    chore(voiceSearch): remove 'else'

--- packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
+++ packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
@@ -50,19 +50,18 @@ const getDefaultButtonInnerElement = (
         <line x1="8" y1="23" x2="16" y2="23" />
       </>
     );
-  } else {
-    return (
-      <>
-        <path
-          d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"
-          fill={isListening ? 'currentColor' : ''}
-        />
-        <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
-        <line x1="12" y1="19" x2="12" y2="23" />
-        <line x1="8" y1="23" x2="16" y2="23" />
-      </>
-    );
   }
+  return (
+    <>
+      <path
+        d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"
+        fill={isListening ? 'currentColor' : ''}
+      />
+      <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+      <line x1="12" y1="19" x2="12" y2="23" />
+      <line x1="8" y1="23" x2="16" y2="23" />
+    </>
+  );
 };
 
 const DefaultButton: React.FC<InnerComponentProps> = ({
