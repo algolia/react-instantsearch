70a01c5e591e5690c7170d25c0312427c308d527
Haroen Viaene <hello@haroen.me>
2019-04-18 10:25:41 +0200

    fix tests

--- packages/react-instantsearch-core/src/core/utils.ts
+++ packages/react-instantsearch-core/src/core/utils.ts
@@ -24,6 +24,7 @@ export const shallowEqual = (objA, objB) => {
 
   // Test for A's keys different from B.
   const hasOwn = Object.prototype.hasOwnProperty;
+  // tslint:disable:prefer-for-of
   for (let i = 0; i < keysA.length; i++) {
     if (!hasOwn.call(objB, keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
       return false;
