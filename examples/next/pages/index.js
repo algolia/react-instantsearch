85e2f77f0db8baf0859c9e99f143fc490b338063
Samuel Vaillant <samuel.vllnt@gmail.com>
2019-04-29 11:23:19 +0200

    fix(compat): upgrade Next.js example lifecycles (#2379)
    * fix(compat): update Next example
    
    * fix(compat): use last Next router

--- examples/next/pages/index.js
+++ examples/next/pages/index.js
@@ -1,6 +1,7 @@
+import { isEqual } from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
-import Router from 'next/router';
+import { withRouter } from 'next/router';
 import qs from 'qs';
 import { Head, App, findResultsState } from '../components';
 
@@ -8,65 +9,72 @@ const updateAfter = 700;
 
 const createURL = state => `?${qs.stringify(state)}`;
 
-const searchStateToUrl = searchState =>
+const pathToSearchState = path =>
+  path.includes('?') ? qs.parse(path.substring(path.indexOf('?') + 1)) : {};
+
+const searchStateToURL = searchState =>
   searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : '';
 
-export default class extends React.Component {
+class Page extends React.Component {
   static propTypes = {
+    router: PropTypes.object.isRequired,
     resultsState: PropTypes.object,
     searchState: PropTypes.object,
   };
 
   state = {
     searchState: this.props.searchState,
+    lastRouter: this.props.router,
   };
 
-  /*
-     nextjs params.query doesn't handle nested objects
-     once it does, params.query could be used directly here, but also inside the constructor
-     to initialize the searchState.
-  */
-  static async getInitialProps(params) {
-    const searchState = params.asPath.includes('?')
-      ? qs.parse(params.asPath.substring(params.asPath.indexOf('?') + 1))
-      : {};
+  static async getInitialProps({ asPath }) {
+    const searchState = pathToSearchState(asPath);
     const resultsState = await findResultsState(App, { searchState });
-    return { resultsState, searchState };
+
+    return {
+      resultsState,
+      searchState,
+    };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    if (!isEqual(state.lastRouter, props.router)) {
+      return {
+        searchState: pathToSearchState(props.router.asPath),
+        lastRouter: props.router,
+      };
+    }
+
+    return null;
   }
 
   onSearchStateChange = searchState => {
     clearTimeout(this.debouncedSetState);
+
     this.debouncedSetState = setTimeout(() => {
-      const href = searchStateToUrl(searchState);
-      Router.push(href, href, {
+      const href = searchStateToURL(searchState);
+
+      this.props.router.push(href, href, {
         shallow: true,
       });
     }, updateAfter);
+
     this.setState({ searchState });
   };
-
-  componentDidMount() {
-    this.setState({ searchState: qs.parse(window.location.search.slice(1)) });
-  }
-
-  componentWillReceiveProps() {
-    // @TODO: probably derived state
-    this.setState({ searchState: qs.parse(window.location.search.slice(1)) });
-  }
 
   render() {
     return (
       <div>
         <Head title="Home" />
-        <div>
-          <App
-            searchState={this.state.searchState}
-            resultsState={this.props.resultsState}
-            onSearchStateChange={this.onSearchStateChange}
-            createURL={createURL}
-          />
-        </div>
+        <App
+          searchState={this.state.searchState}
+          resultsState={this.props.resultsState}
+          onSearchStateChange={this.onSearchStateChange}
+          createURL={createURL}
+        />
       </div>
     );
   }
 }
+
+export default withRouter(Page);
