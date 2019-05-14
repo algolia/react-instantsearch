bf5d46b3b1419a61951ba69762b852f92b3d2910
Eunjae Lee <eunjae.lee@algolia.com>
2019-05-14 17:10:50 +0200

    chore(voiceSearch): change code using Fragment

--- packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
+++ packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
@@ -24,41 +24,12 @@ type VoiceSearchProps = {
   statusComponent?: React.FC<InnerComponentProps>;
 };
 
-const getDefaultButtonInnerElement = (
-  status: string,
-  errorCode: string | undefined,
-  isListening: boolean
-) => {
-  if (status === 'error' && errorCode === 'not-allowed') {
-    return (
-      <>
-        <line x1="1" y1="1" x2="23" y2="23" />
-        <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />
-        <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />
-        <line x1="12" y1="19" x2="12" y2="23" />
-        <line x1="8" y1="23" x2="16" y2="23" />
-      </>
-    );
-  }
-  return (
-    <>
-      <path
-        d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"
-        fill={isListening ? 'currentColor' : ''}
-      />
-      <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
-      <line x1="12" y1="19" x2="12" y2="23" />
-      <line x1="8" y1="23" x2="16" y2="23" />
-    </>
-  );
-};
-
 const DefaultButton: React.FC<InnerComponentProps> = ({
   status,
   errorCode,
   isListening,
 }) => {
-  return (
+  return status === 'error' && errorCode === 'not-allowed' ? (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="16"
@@ -70,7 +41,31 @@ const DefaultButton: React.FC<InnerComponentProps> = ({
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      {getDefaultButtonInnerElement(status, errorCode, isListening)}
+      <line x1="1" y1="1" x2="23" y2="23" />
+      <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />
+      <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />
+      <line x1="12" y1="19" x2="12" y2="23" />
+      <line x1="8" y1="23" x2="16" y2="23" />
+    </svg>
+  ) : (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path
+        d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"
+        fill={isListening ? 'currentColor' : ''}
+      />
+      <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+      <line x1="12" y1="19" x2="12" y2="23" />
+      <line x1="8" y1="23" x2="16" y2="23" />
     </svg>
   );
 };
