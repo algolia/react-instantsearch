896e17474b167020bbd2565b6661a85b69993fcd
Haroen Viaene <hello@haroen.me>
2019-05-16 18:01:03 +0200

    refactor(omit): use internal (babel) replacement

--- packages/react-instantsearch-core/src/core/utils.ts
+++ packages/react-instantsearch-core/src/core/utils.ts
@@ -62,3 +62,20 @@ export function addQueryID(hits, queryID) {
     __queryID: queryID,
   }));
 }
+
+// https://github.com/babel/babel/blob/3aaafae053fa75febb3aa45d45b6f00646e30ba4/packages/babel-helpers/src/helpers.js#L604-L620
+export function omit(source: { [key: string]: any }, excluded: string[]) {
+  if (source === null || source === undefined) {
+    return {};
+  }
+  const target = {};
+  const sourceKeys = Object.keys(source);
+  for (let i = 0; i < sourceKeys.length; i++) {
+    const key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) {
+      continue;
+    }
+    target[key] = source[key];
+  }
+  return target;
+}
