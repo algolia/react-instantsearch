9977f733cb6c36d1c7854fae5e40a76c34218881
Eunjae Lee <eunjae.lee@algolia.com>
2019-05-16 16:23:35 +0200

    type(voiceSearch): add accessibility modifier

--- packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
+++ packages/react-instantsearch-dom/src/components/VoiceSearch.tsx
@@ -129,7 +129,7 @@ class VoiceSearch extends Component<VoiceSearchProps, VoiceListeningState> {
     );
   }
 
-  componentWillUnmount() {
+  public componentWillUnmount() {
     this.voiceSearchHelper.dispose();
   }
 
