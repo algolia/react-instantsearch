574de2b460c8a70334dbb39bd760fe7154393071
Yannick Croissant <yannick.croissant@gmail.com>
2019-06-05 16:12:37 +0200

    refactor(root): remove unused InstantSearch component

--- packages/react-instantsearch-native/src/index.ts
+++ packages/react-instantsearch-native/src/index.ts
@@ -6,7 +6,6 @@ export { translatable } from 'react-instantsearch-core';
 // Widget
 export { Configure } from 'react-instantsearch-core';
 export { QueryRuleContext } from 'react-instantsearch-core';
-export { default as InstantSearch } from './widgets/InstantSearch';
 export { Index } from 'react-instantsearch-core';
 
 // Connectors
