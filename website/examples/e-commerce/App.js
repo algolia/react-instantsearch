59127cf2d755ad022ee4153e56c79252e785cb2f
François Chalifour <francois.chalifour@gmail.com>
2019-06-10 15:07:03 +0200

    fix: store Hit component

--- website/examples/e-commerce/App.js
+++ website/examples/e-commerce/App.js
@@ -27,6 +27,43 @@ const searchClient = algoliasearch(
   '6be0576ff61c053d5f9a3225e2a90f76'
 );
 
+const Hit = ({ hit }) => (
+  <article className="hit">
+    <header className="hit-image-container">
+      <img src={hit.image} alt={hit.name} className="hit-image" />
+    </header>
+
+    <p className="hit-category">{hit.categories[0]}</p>
+    <h1>
+      <Highlight attribute="name" tagName="mark" hit={hit} />
+    </h1>
+    <p className="hit-description">
+      <Snippet attribute="description" tagName="mark" hit={hit} />
+    </p>
+
+    <footer>
+      <p>
+        <span className="hit-em">$</span> <strong>{hit.price}</strong>{' '}
+        <span className="hit-em hit-rating">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="8"
+            height="8"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill="#e2a400"
+              fillRule="evenodd"
+              d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"
+            />
+          </svg>{' '}
+          {hit.rating}
+        </span>
+      </p>
+    </footer>
+  </article>
+);
+
 const App = props => (
   <InstantSearch
     searchClient={searchClient}
@@ -185,44 +222,7 @@ const App = props => (
           />
         </header>
 
-        <Hits
-          hitComponent={({ hit }) => (
-            <article className="hit">
-              <header className="hit-image-container">
-                <img src={hit.image} alt={hit.name} className="hit-image" />
-              </header>
-
-              <p className="hit-category">{hit.categories[0]}</p>
-              <h1>
-                <Snippet attribute="name" tagName="mark" hit={hit} />
-              </h1>
-              <p className="hit-description">
-                <Snippet attribute="description" tagName="mark" hit={hit} />
-              </p>
-
-              <footer>
-                <p>
-                  <span className="hit-em">$</span> <strong>{hit.price}</strong>{' '}
-                  <span className="hit-em hit-rating">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="8"
-                      height="8"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fill="#e2a400"
-                        fillRule="evenodd"
-                        d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"
-                      />
-                    </svg>{' '}
-                    {hit.rating}
-                  </span>
-                </p>
-              </footer>
-            </article>
-          )}
-        />
+        <Hits hitComponent={Hit} />
 
         <footer className="container-footer">
           <Pagination
