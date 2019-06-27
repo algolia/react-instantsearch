0f1e6b989f36219e4666dca62719342b707b4c1b
François Chalifour <francois.chalifour@gmail.com>
2019-06-27 15:50:23 +0200

    chore(examples): unify styles

--- website/examples/e-commerce/App.js
+++ website/examples/e-commerce/App.js
@@ -248,6 +248,7 @@ const App = props => {
         <section className="container-results">
           <header className="container-header container-options">
             <SortBy
+              className="container-option"
               defaultRefinement="instant_search"
               items={[
                 {
@@ -266,6 +267,7 @@ const App = props => {
             />
 
             <HitsPerPage
+              className="container-option"
               items={[
                 {
                   label: '16 hits per page',
