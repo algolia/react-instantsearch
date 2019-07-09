algolia/react-instantsearch

commit 9bc2353d60433843d1976c80df16e5c342b2eaef

refactor(lodash): replace isEqual (#2467)
* refactor(lodash): replace isEqual

1. specified function for maps

IFW-735

* refactor(lodash): replace isEqual with fast-deep-equal

packages/react-instantsearch-dom-maps/src/Connector.js
@@ -1,9 +1,25 @@
-import { isEqual } from 'lodash';
 import { Component } from 'react';
 import { polyfill } from 'react-lifecycles-compat';
 import PropTypes from 'prop-types';
 import { connectGeoSearch } from 'react-instantsearch-dom';
 import { LatLngPropType, BoundingBoxPropType } from './propTypes';
+
+function isEqualPosition(a, b) {
+  if (a === b) {
+    return true;
+  }
+  return a.lat === b.lat && a.lng === b.lng;
+}
+
+function isEqualCurrentRefinement(a, b) {
+  if (a === b) {
+    return true;
+  }
+  return (
+    isEqualPosition(a.northEast, b.northEast) &&
+    isEqualPosition(a.southWest, b.southWest)
+  );
+}
 
 export class Connector extends Component {
   static propTypes = {
@@ -20,8 +36,8 @@ export class Connector extends Component {
     const { position, currentRefinement } = props;
     const { previousPosition, previousCurrentRefinement } = state;
 
-    const positionChanged = !isEqual(previousPosition, position);
-    const currentRefinementChanged = !isEqual(
+    const positionChanged = !isEqualPosition(previousPosition, position);
+    const currentRefinementChanged = !isEqualCurrentRefinement(
       previousCurrentRefinement,
       currentRefinement
     );

packages/react-instantsearch-core/src/core/createConnector.tsx
@@ -1,5 +1,5 @@
-import { isEqual } from 'lodash';
 import React, { Component, ReactType } from 'react';
+import isEqual from 'fast-deep-equal';
 import { shallowEqual, getDisplayName, removeEmptyKey } from './utils';
 import {
   InstantSearchConsumer,

commit 8453ac912d5f60caeebc4310df8b4938d15a0b68

fix(geo): check for undefined in isEqual
issue introduced in #2467

IFW-851

packages/react-instantsearch-dom-maps/src/Connector.js
@@ -8,6 +8,10 @@ function isEqualPosition(a, b) {
   if (a === b) {
     return true;
   }
+  if (a === undefined || b === undefined) {
+    return false;
+  }
+
   return a.lat === b.lat && a.lng === b.lng;
 }
 
@@ -15,6 +19,11 @@ function isEqualCurrentRefinement(a, b) {
   if (a === b) {
     return true;
   }
+
+  if (a === undefined || b === undefined) {
+    return false;
+  }
+
   return (
     isEqualPosition(a.northEast, b.northEast) &&
     isEqualPosition(a.southWest, b.southWest)