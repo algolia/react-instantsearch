5215875ed0dd5c9581d1b5ee69d697ce6097870e
François Chalifour <francois.chalifour@gmail.com>
2020-01-30 13:20:40 +0100

    poc(relatedItems): showcase Related Items strategies

--- stories/ConfigureRelatedItems.stories.tsx
+++ stories/ConfigureRelatedItems.stories.tsx
@@ -13,17 +13,108 @@ type Hit = any;
 
 const stories = storiesOf('ConfigureRelatedItems', module);
 
-stories.add('default', () => <ConfigureRelatedItemsExample />);
+const baseHit = {
+  sport_en: 'ROAD CYCLING',
+  sport_pratice_en: 'ROAD CYCLING',
+  nature_id_en: 'Road bike',
+  gender_id_en: 'ADULT',
+  business_color: '#FFFFFE',
+  category_id: ['2', '1001', '1320', '1320', '6105', '10504', '10505'],
+  image_link_small_en:
+    'http://media.decathlon.sg/1665586-home_default/triban-520-road-bike-black-white.jpg',
+  link_en: '/p/8389938-triban-520-road-bike-black-white.html',
+  manufacturer_en: "B'Twin",
+  name_en: 'Road Bike Triban 520 (Black/White)',
+  price: 550.000001,
+  reference: '8389938',
+  item_codes: '2241007,2241008,2241009,2241010',
+  price_nodiscount: 650.000001,
+  clearance: 'Clearance',
+  variations: [
+    {
+      id_code_model: '8389938',
+      id_code_article: '2241007',
+      stock: 6,
+      size: 'XS',
+    },
+    {
+      id_code_model: '8389938',
+      id_code_article: '2241008',
+      stock: 3,
+      size: 'S',
+    },
+    {
+      id_code_model: '8389938',
+      id_code_article: '2241009',
+      stock: 11,
+      size: 'M',
+    },
+    {
+      id_code_model: '8389938',
+      id_code_article: '2241010',
+      stock: 8,
+      size: 'L',
+    },
+  ],
+  objectID: '8389938',
+};
+
+stories.add('with optionalFilters', () => (
+  <ConfigureRelatedItemsExample
+    relatedItemsConfig={{
+      hit: baseHit,
+      matchingPatterns: {
+        sport_en: { score: 2 },
+        nature_id_en: { score: 1 },
+        gender_id_en: { score: 3 },
+      },
+    }}
+  />
+));
+stories.add('with personalization', () => (
+  <ConfigureRelatedItemsExample
+    relatedItemsConfig={{
+      hit: baseHit,
+      matchingPatterns: {},
+      transformSearchParameters: searchParameters => {
+        return {
+          ...searchParameters,
+          enablePersonalization: true,
+          userToken: 'anonymous-4c418333-3964-4f7d-ae27-02ea7729c439',
+        };
+      },
+    }}
+  />
+));
+stories.add('with optionaFilters and personalization', () => (
+  <ConfigureRelatedItemsExample
+    relatedItemsConfig={{
+      hit: baseHit,
+      matchingPatterns: {
+        sport_en: { score: 2 },
+        nature_id_en: { score: 1 },
+        gender_id_en: { score: 3 },
+      },
+      transformSearchParameters: searchParameters => {
+        return {
+          ...searchParameters,
+          enablePersonalization: true,
+          userToken: 'anonymous-4c418333-3964-4f7d-ae27-02ea7729c439',
+        };
+      },
+    }}
+  />
+));
 
 function RelatedHit({ hit }: { hit: Hit }) {
   return (
     <div>
       <div className="ais-RelatedHits-item-image">
-        <img src={hit.image} alt={hit.name} />
+        <img src={hit.image_link_small_en} alt={hit.name_en} />
       </div>
 
       <div className="ais-RelatedHits-item-title">
-        <h4>{hit.name}</h4>
+        <h4>{hit.name_en}</h4>
       </div>
     </div>
   );
@@ -61,15 +152,15 @@ const NextPagination = connectPagination(
   }
 );
 
-function ConfigureRelatedItemsExample() {
+function ConfigureRelatedItemsExample({ relatedItemsConfig }) {
   const [referenceHit, setReferenceHit] = useState<Hit | null>(null);
 
   const ReferenceHit = React.memo<{ hit: Hit }>(
     ({ hit }) => {
       return (
         <div ref={() => setReferenceHit(hit)}>
-          <img src={hit.image} alt={hit.name} />
-          <h3>{hit.name}</h3>
+          <img src={hit.image_link_small_en} alt={hit.name_en} />
+          <h3>{hit.name_en}</h3>
         </div>
       );
     },
@@ -79,24 +170,22 @@ function ConfigureRelatedItemsExample() {
   );
 
   return (
-    <WrapWithHits linkedStoryGroup="ConfigureRelatedItems">
-      <Index indexName="instant_search" indexId="mainIndex">
+    <WrapWithHits
+      linkedStoryGroup="ConfigureRelatedItems"
+      indexName="prod_singapore"
+      appId="LR431A4C5G"
+      apiKey="41fed36d8b52d65a9b9e7475a267319a"
+    >
+      <Index indexName="prod_singapore" indexId="mainIndex">
         <Configure hitsPerPage={1} />
 
         <Hits hitComponent={ReferenceHit} />
       </Index>
 
       {referenceHit && (
-        <Index indexName="instant_search" indexId="relatedIndex">
+        <Index indexName="prod_singapore" indexId="relatedIndex">
           <Configure hitsPerPage={4} />
-          <ExperimentalConfigureRelatedItems
-            hit={referenceHit}
-            matchingPatterns={{
-              brand: { score: 3 },
-              type: { score: 10 },
-              categories: { score: 2 },
-            }}
-          />
+          <ExperimentalConfigureRelatedItems {...relatedItemsConfig} />
 
           <h2>Related items</h2>
 
