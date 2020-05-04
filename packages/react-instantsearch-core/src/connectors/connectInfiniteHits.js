38ac4394eef28afd8b26b519d87122b0fcd0bc54
eunjae-lee <karis612@gmail.com>
2020-05-04 14:56:36 +0200

    feat(infinite-hits): return cached hits at initial render before search

--- packages/react-instantsearch-core/src/connectors/connectInfiniteHits.js
+++ packages/react-instantsearch-core/src/connectors/connectInfiniteHits.js
@@ -50,6 +50,14 @@ function getInMemoryCache() {
   };
 }
 
+function flattenHits(cachedHits) {
+  return Object.keys(cachedHits)
+    .sort()
+    .reduce((acc, page) => {
+      return acc.concat(cachedHits[page]);
+    }, []);
+}
+
 /**
  * InfiniteHits connector provides the logic to create connected
  * components that will render an continuous list of results retrieved from
@@ -78,7 +86,7 @@ export default createConnector({
 
     if (!results) {
       return {
-        hits: [],
+        hits: flattenHits(this._cachedHits),
         hasPrevious: false,
         hasMore: false,
         refine: () => {},
@@ -116,11 +124,6 @@ export default createConnector({
     */
     const firstReceivedPage = Math.min(...Object.keys(this._cachedHits));
     const lastReceivedPage = Math.max(...Object.keys(this._cachedHits));
-    const flattenHits = Object.keys(this._cachedHits)
-      .sort()
-      .reduce((acc, _page) => {
-        return acc.concat(this._cachedHits[_page]);
-      }, []);
 
     const hasPrevious = firstReceivedPage > 0;
     const lastPageIndex = nbPages - 1;
@@ -129,7 +132,7 @@ export default createConnector({
     const refineNext = event => this.refine(event, lastReceivedPage + 1);
 
     return {
-      hits: flattenHits,
+      hits: flattenHits(this._cachedHits),
       hasPrevious,
       hasMore,
       refinePrevious,
