c5e7d91c63cae0238a126b58900ce064f0102bda
Nuno Maduro <enunomaduro@gmail.com>
2020-05-15 13:20:20 +0200

    fix(call-stack-size-bug): uses demo like a customer would use

--- examples/server-side-rendering/src/createApp.js
+++ examples/server-side-rendering/src/createApp.js
@@ -1,13 +1,16 @@
 import algoliasearch from 'algoliasearch/lite';
+// import { createInMemoryCache } from '@algolia/cache-in-memory';
 import App from './App';
-
-export const createApp = () => {
-  const indexName = 'instant_search';
   const searchClient = algoliasearch(
     'latency',
-    '6be0576ff61c053d5f9a3225e2a90f76'
+    '6be0576ff61c053d5f9a3225e2a90f76',
+    { // For testing with cache, uncomment cache related lines:
+          // responsesCache: createInMemoryCache(),
+          // requestsCache: createInMemoryCache({ serializable: false })
+    }
   );
-
+export const createApp = () => {
+  const indexName = 'instant_search';
   const props = {
     indexName,
     searchClient,
