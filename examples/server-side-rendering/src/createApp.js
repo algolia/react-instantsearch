800590404d05d1aa08a2d8d30b8d2560c8595a72
Nuno Maduro <enunomaduro@gmail.com>
2020-05-15 13:29:16 +0200

    fix(max-call-stack-size): cs

--- examples/server-side-rendering/src/createApp.js
+++ examples/server-side-rendering/src/createApp.js
@@ -1,14 +1,15 @@
 import algoliasearch from 'algoliasearch/lite';
 // import { createInMemoryCache } from '@algolia/cache-in-memory';
 import App from './App';
-  const searchClient = algoliasearch(
-    'latency',
-    '6be0576ff61c053d5f9a3225e2a90f76',
-    { // For testing with cache, uncomment cache related lines:
-          // responsesCache: createInMemoryCache(),
-          // requestsCache: createInMemoryCache({ serializable: false })
-    }
-  );
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76',
+  {
+    // For testing with cache, uncomment cache related lines:
+    // responsesCache: createInMemoryCache(),
+    // requestsCache: createInMemoryCache({ serializable: false })
+  }
+);
 export const createApp = () => {
   const indexName = 'instant_search';
   const props = {
