c0b27939ded2603f3572581869a3e853c762b9e7
Nuno Maduro <enunomaduro@gmail.com>
2020-05-18 10:58:45 +0200

    fix(max-call-stack-size): improves example

--- examples/server-side-rendering/src/createApp.js
+++ examples/server-side-rendering/src/createApp.js
@@ -1,15 +1,12 @@
 import algoliasearch from 'algoliasearch/lite';
-// import { createInMemoryCache } from '@algolia/cache-in-memory';
+
 import App from './App';
+
 const searchClient = algoliasearch(
   'latency',
-  '6be0576ff61c053d5f9a3225e2a90f76',
-  {
-    // For testing with cache, uncomment cache related lines:
-    // responsesCache: createInMemoryCache(),
-    // requestsCache: createInMemoryCache({ serializable: false })
-  }
+  '6be0576ff61c053d5f9a3225e2a90f76'
 );
+
 export const createApp = () => {
   const indexName = 'instant_search';
   const props = {
