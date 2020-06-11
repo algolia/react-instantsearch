eda06a130ee139611556946d36010bd1f33ea754
François Chalifour <francois.chalifour@gmail.com>
2020-06-11 14:16:20 +0200

    test(examples): wrap `App` in `Router`

--- examples/react-router/src/App.test.js
+++ examples/react-router/src/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
+
 import App from './App';
 
 describe('react-router recipe', () => {
   it('App renders without crashing', () => {
-    const component = renderer.create(<App />);
+    const component = renderer.create(
+      <Router>
+        <Route path="/" component={App} />
+      </Router>
+    );
 
     expect(component.toJSON()).toMatchSnapshot();
   });
