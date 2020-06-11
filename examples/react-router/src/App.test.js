dfb0fcdf84ff42d6a123e1ba9ec677f20fcdf630
François Chalifour <francois.chalifour@gmail.com>
2020-06-11 14:53:17 +0200

    test(examples): inject `history` to `Router`

--- examples/react-router/src/App.test.js
+++ examples/react-router/src/App.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import { createMemoryHistory } from 'history';
 
 import App from './App';
+
+const history = createMemoryHistory('/');
 
 describe('react-router recipe', () => {
   it('App renders without crashing', () => {
     const component = renderer.create(
-      <Router>
+      <Router history={history}>
         <Route path="/" component={App} />
       </Router>
     );
