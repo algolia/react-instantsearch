2ba4395f120e797775a911bc52c3acbb7f6e4eaf
François Chalifour <francoischalifour@users.noreply.github.com>
2021-09-27 12:16:08 +0200

    fix: use ESM import

--- packages/react-instantsearch-core-next/src/useIndex.ts
+++ packages/react-instantsearch-core-next/src/useIndex.ts
@@ -1,4 +1,4 @@
-import index from 'instantsearch.js/cjs/widgets/index/index';
+import index from 'instantsearch.js/es/widgets/index/index';
 import { useEffect, useMemo } from 'react';
 
 import { useIndexContext } from './useIndexContext';
