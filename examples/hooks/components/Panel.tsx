c3a683fea4391470619245fcda4fb42530d69e7e
Haroen Viaene <hello@haroen.me>
2021-11-23 10:48:07 +0100

    redivide
    Co-authored-by: François Chalifour <francoischalifour@users.noreply.github.com>

--- examples/hooks/components/Panel.tsx
+++ examples/hooks/components/Panel.tsx
@@ -11,7 +11,7 @@ export function Panel({
 }) {
   return (
     <div className="ais-Panel">
-      {header && <h2 className="ais-Panel-header">{header}</h2>}
+      {header && <div className="ais-Panel-header">{header}</div>}
       <div className="ais-Panel-body">{children}</div>
       {footer && <div className="ais-Panel-footer">{footer}</div>}
     </div>
