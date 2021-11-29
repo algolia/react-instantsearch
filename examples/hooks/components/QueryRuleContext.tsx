b17eebcacc580a58f8cc77a2c37b17dc518e4f5c
Youcef Mammar <youcef.mammar@algolia.com>
2021-11-29 19:10:24 +0100

    fix: linting

--- examples/hooks/components/QueryRuleContext.tsx
+++ examples/hooks/components/QueryRuleContext.tsx
@@ -1,6 +1,9 @@
 import { useQueryRules, UseQueryRulesProps } from 'react-instantsearch-hooks';
 
-export type QueryRuleContextProps = Pick<UseQueryRulesProps, 'trackedFilters' | 'transformRuleContexts'>;
+export type QueryRuleContextProps = Pick<
+  UseQueryRulesProps,
+  'trackedFilters' | 'transformRuleContexts'
+>;
 
 export function QueryRuleContext(props: QueryRuleContextProps) {
   useQueryRules(props);
