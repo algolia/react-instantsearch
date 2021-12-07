f967f4e63355ea77b18cba727c641f083392a44d
Dhaya <154633+dhayab@users.noreply.github.com>
2021-12-07 10:19:24 +0100

    fix: make tabs accessible

--- examples/hooks/components/layout/Tabs.tsx
+++ examples/hooks/components/layout/Tabs.tsx
@@ -8,41 +8,70 @@ export type TabProps = {
   title: string;
 };
 
+const getTabKey = (index: number, suffix?: string) =>
+  [`tab-${index}`, suffix].filter(Boolean).join('-');
+
 export function Tabs({ children }) {
   const [currentTab, setCurrentTab] = useState(0);
+  const tabsRefs: HTMLElement[] = [];
+
+  const onKeyPress = (event: React.KeyboardEvent) => {
+    let tabIndex;
+    switch (event.key) {
+      case 'ArrowLeft':
+        tabIndex = Math.max(0, currentTab - 1);
+        break;
+      case 'ArrowRight':
+        tabIndex = Math.min(currentTab + 1, React.Children.count(children) - 1);
+        break;
+      default:
+        return;
+    }
+
+    tabsRefs[tabIndex].focus();
+    setCurrentTab(tabIndex);
+  };
 
   return (
     <div className="Tabs">
-      <ul className="Tabs-header">
+      <div role="tablist" className="Tabs-header">
         {React.Children.map<React.ReactChild, React.ReactElement<TabProps>>(
           children,
           (child, index) => (
-            <li
+            <button
+              role="tab"
+              aria-selected={currentTab === index}
+              aria-controls={getTabKey(index, 'item')}
+              id={getTabKey(index, 'title')}
+              tabIndex={currentTab !== index ? -1 : 0}
               className={cx(
                 'Tabs-title',
                 currentTab === index && 'Tabs-title--active'
               )}
-              key={index}
+              ref={(element) => (tabsRefs[index] = element!)}
+              key={getTabKey(index)}
               onClick={() => setCurrentTab(index)}
+              onKeyDown={(event) => onKeyPress(event)}
             >
               {child.props.title}
-            </li>
+            </button>
           )
         )}
-      </ul>
-      <ul className="Tabs-list">
+      </div>
+      <div className="Tabs-list">
         {React.Children.map(children, (child, index) => (
-          <li
-            className={cx(
-              'Tabs-item',
-              currentTab === index && 'Tabs-item--active'
-            )}
-            key={index}
+          <div
+            tabIndex={0}
+            role="tabpanel"
+            id={getTabKey(index, 'item')}
+            aria-labelledby={getTabKey(index, 'title')}
+            hidden={currentTab !== index}
+            key={getTabKey(index)}
           >
             {child}
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
