6abd1ef81f095f1ca14537b57a9b53835161510a
Haroen Viaene <hello@haroen.me>
2021-12-14 14:32:47 +0100

    test: add an assertion to make sure it actually exports a module

---
+++ packages/react-instantsearch-hooks-server/test/is-a-module.mjs
@@ -0,0 +1,5 @@
+import assert from 'assert';
+
+import * as ReactInstantSearchHooksServer from 'react-instantsearch-hooks-server';
+
+assert.ok(ReactInstantSearchHooksServer);
---
+++ packages/react-instantsearch-hooks/test/is-a-module.mjs
@@ -0,0 +1,5 @@
+import assert from 'assert';
+
+import * as ReactInstantSearchHooks from 'react-instantsearch-hooks';
+
+assert.ok(ReactInstantSearchHooks);
