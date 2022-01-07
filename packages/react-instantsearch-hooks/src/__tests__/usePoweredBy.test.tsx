8d10d50eb67d7cb0a1646f1b27fd609771de8a1b
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-01-07 22:59:34 +0100

    refactor: drop useConnector usage

--- packages/react-instantsearch-hooks/src/__tests__/usePoweredBy.test.tsx
+++ packages/react-instantsearch-hooks/src/__tests__/usePoweredBy.test.tsx
@@ -1,26 +1,13 @@
-import { renderHook } from '@testing-library/react-hooks';
-
-import { createInstantSearchTestWrapper } from '../../../../test/utils';
 import { usePoweredBy } from '../usePoweredBy';
 
 describe('usePoweredBy', () => {
   test('returns the connector render state', async () => {
-    const hostname = window.location?.hostname || '';
-    const wrapper = createInstantSearchTestWrapper();
-    const { result, waitForNextUpdate } = renderHook(() => usePoweredBy(), {
-      wrapper,
-    });
+    const { url } = usePoweredBy();
 
-    // Initial render state from manual `getWidgetRenderState`
-    expect(result.current).toEqual({
-      url: `https://www.algolia.com/?utm_source=react-instantsearch&utm_medium=website&utm_content=${hostname}&utm_campaign=poweredby`,
-    });
-
-    await waitForNextUpdate();
-
-    // InstantSearch.js state from the `render` lifecycle step
-    expect(result.current).toEqual({
-      url: `https://www.algolia.com/?utm_source=react-instantsearch&utm_medium=website&utm_content=${hostname}&utm_campaign=poweredby`,
-    });
+    expect(url).toBe(
+      `https://www.algolia.com/?utm_source=react-instantsearch&utm_medium=website&utm_content=${
+        window.location?.hostname || ''
+      }&utm_campaign=poweredby`
+    );
   });
 });
