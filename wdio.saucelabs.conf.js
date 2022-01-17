0d566501ec8fbd2f052b2b471200ccf64691b0d2
Dhaya <154633+dhayab@users.noreply.github.com>
2022-01-17 16:50:22 +0100

    use updated instantsearch-e2e-tests package

--- wdio.saucelabs.conf.js
+++ wdio.saucelabs.conf.js
@@ -1,21 +1,3 @@
 const { saucelabs } = require('instantsearch-e2e-tests');
 
-exports.config = {
-  ...saucelabs,
-  reporters: [
-    'spec',
-    [
-      'junit',
-      {
-        outputDir: `${__dirname}/junit/wdio`,
-        outputFileFormat({
-          cid,
-          capabilities: { browserName, browserVersion },
-        }) {
-          return `results-${cid}.${browserName}-${browserVersion}.xml`;
-        },
-        addFileAttribute: true,
-      },
-    ],
-  ],
-};
+exports.config = saucelabs;
