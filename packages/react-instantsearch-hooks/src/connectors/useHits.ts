2249f969b006610d6ae2439506bda8ce897b1a4b
Haroen Viaene <hello@haroen.me>
2022-02-23 14:16:52 +0100

    use BaseHit from IS

--- packages/react-instantsearch-hooks/src/connectors/useHits.ts
+++ packages/react-instantsearch-hooks/src/connectors/useHits.ts
@@ -3,19 +3,17 @@ import connectHits from 'instantsearch.js/es/connectors/hits/connectHits';
 import { useConnector } from '../hooks/useConnector';
 
 import type { AdditionalWidgetProperties } from '../hooks/useConnector';
+import type { BaseHit } from 'instantsearch.js';
 import type {
   HitsConnectorParams,
   HitsWidgetDescription,
   HitsConnector,
 } from 'instantsearch.js/es/connectors/hits/connectHits';
 
-export type UseHitsProps<
-  THit extends Record<string, unknown> = Record<string, unknown>
-> = HitsConnectorParams<THit>;
+export type UseHitsProps<THit extends BaseHit = BaseHit> =
+  HitsConnectorParams<THit>;
 
-export function useHits<
-  THit extends Record<string, unknown> = Record<string, unknown>
->(
+export function useHits<THit extends BaseHit = BaseHit>(
   props?: UseHitsProps<THit>,
   additionalWidgetProperties?: AdditionalWidgetProperties
 ) {
--- packages/react-instantsearch-hooks/src/connectors/useInfiniteHits.ts
+++ packages/react-instantsearch-hooks/src/connectors/useInfiniteHits.ts
@@ -3,19 +3,17 @@ import connectInfiniteHits from 'instantsearch.js/es/connectors/infinite-hits/co
 import { useConnector } from '../hooks/useConnector';
 
 import type { AdditionalWidgetProperties } from '../hooks/useConnector';
+import type { BaseHit } from 'instantsearch.js';
 import type {
   InfiniteHitsConnectorParams,
   InfiniteHitsWidgetDescription,
   InfiniteHitsConnector,
 } from 'instantsearch.js/es/connectors/infinite-hits/connectInfiniteHits';
 
-export type UseInfiniteHitsProps<
-  THit extends Record<string, unknown> = Record<string, unknown>
-> = InfiniteHitsConnectorParams<THit>;
+export type UseInfiniteHitsProps<THit extends BaseHit = BaseHit> =
+  InfiniteHitsConnectorParams<THit>;
 
-export function useInfiniteHits<
-  THit extends Record<string, unknown> = Record<string, unknown>
->(
+export function useInfiniteHits<THit extends BaseHit = BaseHit>(
   props?: UseInfiniteHitsProps<THit>,
   additionalWidgetProperties?: AdditionalWidgetProperties
 ) {
