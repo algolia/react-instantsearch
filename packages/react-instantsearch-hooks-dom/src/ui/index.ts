9b63c82daf97077d64b1b88217ddf234ed63257b
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-03-02 12:22:21 +0100

    feat: implement SortBy UI component

---
+++ packages/react-instantsearch-hooks-dom/src/ui/index.ts
@@ -0,0 +1,6 @@
+export * from './ClearRefinements';
+export * from './CurrentRefinements';
+export * from './Hits';
+export * from './Pagination';
+export * from './SearchBox';
+export * from './SortBy';
