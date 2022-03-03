afeb265e67317854dd6f852af2c7155e19f549b6
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-03-03 20:02:51 +0100

    refactor: remove barrel

--- packages/react-instantsearch-hooks-dom/src/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './ClearRefinements';
-export * from './CurrentRefinements';
-export * from './Hits';
-export * from './Pagination';
-export * from './SearchBox';
-export * from './SortBy';
