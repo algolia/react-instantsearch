d0e172d4bff0860bc280d993324b3b06fd9f7c22
François Chalifour <francoischalifour@users.noreply.github.com>
2022-03-21 12:44:54 +0100

    feat(hooks): implement <HitsPerPage>

--- packages/react-instantsearch-hooks-dom/src/widgets/index.ts
+++ packages/react-instantsearch-hooks-dom/src/widgets/index.ts
@@ -1,6 +1,7 @@
 export * from './ClearRefinements';
 export * from './CurrentRefinements';
 export * from './Hits';
+export * from './HitsPerPage';
 export * from './Pagination';
 export * from './SearchBox';
 export * from './SortBy';
