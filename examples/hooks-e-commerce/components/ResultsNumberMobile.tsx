fd710da4c68efa162fe8a452fb1ebb33bb638071
Haroen Viaene <hello@haroen.me>
2022-07-07 14:59:42 +0200

    useInstantSearch

--- examples/hooks-e-commerce/components/ResultsNumberMobile.tsx
+++ examples/hooks-e-commerce/components/ResultsNumberMobile.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { connectStats } from 'instantsearch.js/es/connectors';
-import {
-  StatsConnectorParams,
-  StatsWidgetDescription,
-} from 'instantsearch.js/es/connectors/stats/connectStats';
-import { useConnector } from 'react-instantsearch-hooks-web';
+import { useInstantSearch } from 'react-instantsearch-hooks-web';
 import { formatNumber } from '../utils';
 
 export function ResultsNumberMobile() {
-  const { nbHits } = useConnector<StatsConnectorParams, StatsWidgetDescription>(
-    connectStats,
-    {},
-    { $$widgetType: 'e-commerce.stats' }
-  );
+  const {
+    results: { nbHits },
+  } = useInstantSearch();
 
   return (
     <div>
