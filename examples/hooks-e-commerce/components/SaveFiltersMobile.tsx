3f126af551dff8a5a266e773c5cd8de0e790f983
Haroen Viaene <hello@haroen.me>
2022-07-07 14:59:05 +0200

    useInstantSearch

--- examples/hooks-e-commerce/components/SaveFiltersMobile.tsx
+++ examples/hooks-e-commerce/components/SaveFiltersMobile.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { connectStats } from 'instantsearch.js/es/connectors';
-import {
-  StatsConnectorParams,
-  StatsWidgetDescription,
-} from 'instantsearch.js/es/connectors/stats/connectStats';
-import { useConnector } from 'react-instantsearch-hooks-web';
+import { useInstantSearch } from 'react-instantsearch-hooks-web';
 import { formatNumber } from '../utils';
 
 export function SaveFiltersMobile({ onClick }: { onClick: () => void }) {
-  const { nbHits } = useConnector<StatsConnectorParams, StatsWidgetDescription>(
-    connectStats,
-    {},
-    { $$widgetType: 'e-commerce.stats' }
-  );
+  const {
+    results: { nbHits },
+  } = useInstantSearch();
+
   return (
     <button className="button button-primary" onClick={onClick}>
       See {formatNumber(nbHits)} results
