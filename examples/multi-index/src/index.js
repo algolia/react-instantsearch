91790b54ba2cdab9f280ee46fe5beafe6873c249
Fabien Motte <fabien.motte@algolia.com>
2022-07-13 11:55:38 +0200

    chore(examples): update multi-index example

--- examples/multi-index/src/index.js
+++ examples/multi-index/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import 'instantsearch.css/themes/algolia.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(<App />);
