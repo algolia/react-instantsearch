4432c0121a7dce9f1b1f450b8e95e70b59ffdd52
Fabien Motte <fabien.motte@algolia.com>
2022-07-13 12:10:26 +0200

    chore(examples): update react-router example

--- examples/react-router/src/index.js
+++ examples/react-router/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import 'instantsearch.css/themes/algolia.css';
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <Router>
     <Route path="/" component={App} />
-  </Router>,
-  document.getElementById('root')
+  </Router>
 );
