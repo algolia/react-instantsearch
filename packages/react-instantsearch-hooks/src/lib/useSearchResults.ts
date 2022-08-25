ce28d378483ccca6c6bc8dfbe00f0a9878f925a2
Aymeric Giraudet <neadj59@gmail.com>
2022-08-25 16:57:10 +0200

    Update packages/react-instantsearch-hooks/src/lib/useSearchResults.ts
    Co-authored-by: Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>

--- packages/react-instantsearch-hooks/src/lib/useSearchResults.ts
+++ packages/react-instantsearch-hooks/src/lib/useSearchResults.ts
@@ -23,8 +23,9 @@ export function useSearchResults(): SearchResultsApi {
     function handleRender() {
       const results = searchIndex.getResults();
 
-      // results can be null when first search is stalled, in this case we skip the update
-      // see: https://github.com/algolia/instantsearch.js/blob/20996c7a159988c58e00ff24d2d2dc98af8b980f/src/widgets/index/index.ts#L652-L657
+      // Results can be `null` when the first search is stalled.
+      // In this case, we skip the update.
+      // See: https://github.com/algolia/instantsearch.js/blob/20996c7a159988c58e00ff24d2d2dc98af8b980f/src/widgets/index/index.ts#L652-L657
       if (results !== null) {
         setSearchResults({
           results,
